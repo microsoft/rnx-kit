d6aacdb15b7f91f7625dc0e4d3f77cca6ebf629a
Jason Morse <jasonmo@microsoft.com>
2021-01-15 15:20:01 -0800

    add preinstall script to check for using yarn fast, then add command to invoke it

---
+++ scripts/tasks/preinstall.js
@@ -0,0 +1,19 @@
+if (process.env.yarnFast !== "true" && process.stdout.isTTY) {
+  const openRed = `\u001B[91m`;
+  const closeRed = `\u001B[39m`;
+  const openBold = `\u001B[1m`;
+  const closeBold = `\u001B[22m`;
+
+  console.error();
+  console.error(`${openRed}${openBold}`);
+  console.error(
+    "Hi! In order to increase install speed, Midgard uses a yarn fork. Please run `yarn fast` to install dependencies."
+  );
+  console.error(
+    "See https://github.com/VincentBailly/yarn for technical details."
+  );
+  console.error(`${closeBold}${closeRed}`);
+  console.error();
+
+  process.exit(1);
+}
