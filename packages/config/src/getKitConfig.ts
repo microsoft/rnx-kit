293e115755f019279526afe381e3e0b1f7eb9944
Jason Morse <jasonmo@microsoft.com>
2021-01-25 14:37:50 -0800

    add start of config package

---
+++ packages/config/src/getKitConfig.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import { KitConfig } from "./kitConfig";
+import { cosmiconfigSync } from "cosmiconfig";
+
+/**
+ * Options for retrieving a kit config. The default is equivalanet to passing { cwd: process.cwd() }
+ */
+export interface GetKitConfigOptions {
+  /**
+   * Retrieve the kit config options, if they exist, from the specified module.
+   */
+  module?: string;
+
+  /**
+   * Retrive teh kit config options, using the target working directory.
+   */
+  cwd?: string;
+}
+
+export function getKitConfig(options?: GetKitConfigOptions): KitConfig | null {
+  // use a working directory extracted from a module, specified in cwd, or from process.cwd
+  let cwd = options.module
+    ? path.resolve(require.resolve(options.module + "/package.json"), "..")
+    : options.cwd || process.cwd();
+
+  // use the synchronous cosmiconfig load method to see if there is kit info present at the specified location
+  const explorerSync = cosmiconfigSync("rnx-kit");
+  return explorerSync.load(cwd).config;
+}
