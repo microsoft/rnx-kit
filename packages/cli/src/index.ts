2ac5c055056cd35523dc16abb3dcaab24c431086
Adam Foxman <afoxman@microsoft.com>
2021-02-09 11:42:05 -0800

    print out available paths as a test

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,7 +4,9 @@ export const rnxBundle = (
   options: Object
 ) => {
   console.log("It worked!");
+  console.log("__dirname: %s", __dirname);
+  console.log("working directory: %s", process.cwd());
   console.log("argv: %o", argv);
-  console.log("config: %o", config);
+  // console.log("config: %o", config);
   console.log("options: %o", options);
 };
