887eeed6cc499081c3ab253761d3fd7ce880df6c
Adam Foxman <afoxman@microsoft.com>
2021-02-09 10:15:44 -0800

    Add a new cli package with an example command that prints out its args/options.

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,10 @@
+export const rnxBundle = (
+  argv: Array<string>,
+  config /*: ConfigT*/,
+  options: Object
+) => {
+  console.log("It worked!");
+  console.log("argv: %o", argv);
+  console.log("config: %o", config);
+  console.log("options: %o", options);
+};
