cd02858fac26b7b8214fb0eb6776d8e2a0637492
Adam Foxman <adam@thefoxmans.net>
2021-02-12 11:52:40 -0800

    Build system (#36)
    * Add a build system and integrate with existing packages (except metro-config).
    
    * Change files
    
    * Fix test pipeline
    
    * Fix merge typo
    
    * test-app-mobile bundling (#39)
    
    Make metro bundling work using the test-app-mobile package. Add bundling to the CI loop to ensure that it continues to work.
    
    I updated the metro build task so it always runs via the CLI (out-of-proc). There was an in-proc version of the task, but it was failing on a bad transformer, so I removed that for now. If we need in-proc bundling, we can bring the code back and get it working.
    
    I added an option to override the 'assets' directory. The default behavior is to match the bundle directory. react-native-test-app uses this on android bundle runs, and set it to a subdir named 'res'.
    
    I added package.json config for metro bundling, and updated the "scripts" commands for bundling to use rnx-kit-scripts. This ensures that our metro task runs and invokes the metro CLI correctly.
    
    I updated the PR and Build CI loops to run bundling across the repo, ensuring that bundling errors break the build.
    
    * Remove unneeded dependency on Metro in rnx-kit-scripts. Fix test-app-mobile script commands.
    
    * Fix tsconfig.json errors. Remove flow types from test-app-mobile.
    
    * Remove clean from the lage pipeline list and cleanup package.json scripts. Clean is implicitly invoked as part of build.
    
    * Remove windows-specific check that node.exe and repo are on the same drive. Doesn't appear to be needed anymore.

---
+++ scripts/src/configureJust.ts
@@ -0,0 +1,66 @@
+import {
+  task,
+  series,
+  parallel,
+  option,
+  argv,
+  addResolvePath,
+  prettierCheckTask,
+  prettierTask,
+} from "just-scripts";
+
+import { clean } from "./tasks/clean";
+import { depcheck } from "./tasks/depcheck";
+import { eslint } from "./tasks/eslint";
+import { jest } from "./tasks/jest";
+import { metro } from "./tasks/metro";
+import { ts } from "./tasks/ts";
+
+export function configureJust() {
+  //  add a resolve path for the build tooling deps like TS from the scripts folder
+  addResolvePath(__dirname);
+
+  option("production");
+
+  //  metro options
+  option("dev");
+  option("platform", { string: true });
+  option("bundleName", { string: true });
+  option("server");
+  option("port", { number: true });
+
+  // for options that have a check/fix switch this puts them into fix mode
+  option("fix");
+
+  // leaf-level task definitions
+  task("clean", clean);
+  task("depcheck", depcheck);
+  task("lint", eslint);
+  task("jest:default", jest.default);
+  task("jest:ios", jest.ios);
+  task("jest:android", jest.android);
+  task("jest:macos", jest.macos);
+  task("jest:win32", jest.win32);
+  task("jest:windows", jest.windows);
+  task("metro", () => metro);
+  task("no-op", () => {});
+  task("prettier", () => (argv().fix ? prettierTask : prettierCheckTask));
+  task("ts", ts);
+
+  // hierarchical task definintions
+  task("bundle", series("metro"));
+  task("build", series("clean", "lint", "ts"));
+  task("code-style", series("prettier", "lint"));
+  task(
+    "jest:platforms",
+    parallel(
+      "jest:ios",
+      "jest:android",
+      "jest:macos",
+      "jest:win32",
+      "jest:windows"
+    )
+  );
+  task("test", series("jest:default", "jest:platforms"));
+  task("validate", parallel("lint", "test"));
+}
---
+++ scripts/src/tasks/depcheck.js
@@ -0,0 +1,70 @@
+function mergeOneLevel(a, b = {}) {
+  const result = { ...a, ...b };
+  Object.keys(a).forEach((key) => {
+    if (Array.isArray(b[key]) && Array.isArray(a[key])) {
+      result[key] = [].concat(a[key], b[key]);
+    }
+  });
+  return result;
+}
+
+function scriptsDevDeps() {
+  const config = require("rnx-kit-scripts/package.json");
+  return Object.keys(config.devDependencies);
+}
+
+function depcheckTask() {
+  return function(done) {
+    const { logger } = require("just-scripts");
+    const depcheck = require("depcheck");
+    const path = require("path");
+    const config = require(path.join(process.cwd(), "package.json"));
+    const options = mergeOneLevel(
+      {
+        ignorePatterns: ["*eslint*", "/lib/*", "/lib-commonjs/*"],
+        ignoreMatches: ["rnx-kit-scripts", ...scriptsDevDeps()],
+        specials: [
+          depcheck.special.eslint,
+          depcheck.special.webpack,
+          depcheck.special.jest,
+        ],
+      },
+      config.depcheck
+    );
+
+    return depcheck(process.cwd(), options, (result) => {
+      try {
+        if (result.devDependencies.length > 0) {
+          logger.warn("Unused devDependencies");
+          result.devDependencies.forEach((dependency) => {
+            logger.warn(`-- ${dependency}`);
+          });
+        }
+        if (
+          result.dependencies.length > 0 ||
+          Object.keys(result.missing).length > 0
+        ) {
+          if (result.dependencies.length > 0) {
+            logger.error("Unused dependencies");
+            result.dependencies.forEach((dependency) => {
+              logger.error(`-- ${dependency}`);
+            });
+          }
+
+          Object.keys(result.missing).forEach((dependency) => {
+            logger.error(`Missing dependency on ${dependency}`);
+            result.missing[dependency].forEach((file) => {
+              logger.error(`-- ${file}`);
+            });
+          });
+
+          throw "Dependency checking failed";
+        }
+      } catch (error) {
+        done(error);
+      }
+    });
+  };
+}
+
+module.exports.depcheck = depcheckTask;
