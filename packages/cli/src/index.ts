6cb2e43e62dedc7216480034f2e3c2597b95437f
Adam Foxman <afoxman@microsoft.com>
2021-02-14 22:57:59 -0800

    Create a CLI package which self-registers with @react-native-community/cli. Add bundling commands 'rnx-bundle' and 'rnx-start' which invoke metro. Move bundle code from build system to CLI package.

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,101 @@
+import {
+  MetroBundleOptions,
+  MetroStartOptions,
+  metroBundle,
+  metroStart,
+} from "./metro";
+import { getKitConfig, AllPlatforms, BundleParameters } from "@rnx-kit/config";
+import chalk from "chalk";
+
+export function parseBoolean(val: string): boolean {
+  if (val === "false") {
+    return false;
+  }
+  if (val === "true") {
+    return true;
+  }
+  throw new Error(
+    "Invalid boolean value '" + val + "' -- must be true or false"
+  );
+}
+
+export function parsePlatform(val: string): AllPlatforms {
+  if (
+    val === "ios" ||
+    val === "android" ||
+    val === "windows" ||
+    val === "macos"
+  ) {
+    return val;
+  }
+  throw new Error("Invalid platform '" + val + "'");
+}
+
+export function rnxBundle(
+  argv: Array<string>,
+  config /*: ConfigT*/,
+  options: Object
+): void {
+  //  unused params
+  argv;
+  config;
+
+  //  get the rnx kit config, and make sure bundling is enabled
+  const kitConfig = getKitConfig();
+  if (
+    !kitConfig ||
+    kitConfig.bundle === null ||
+    kitConfig.bundle === undefined
+  ) {
+    console.warn(
+      chalk.yellow(
+        "No bundle configuration found for this react-native experience -- skipping bundling"
+      )
+    );
+    return;
+  } else if (!kitConfig.bundle) {
+    console.warn(
+      chalk.yellow(
+        "Bundling is disabled for this react-native experience -- skipping"
+      )
+    );
+    return;
+  }
+  const bundleConfig = kitConfig.bundle;
+
+  //  construct override params from cmd-line options
+  const {
+    entryPath,
+    distPath,
+    assetsPath,
+    bundlePrefix,
+  } = options as BundleParameters;
+  const bundleOverrides: BundleParameters = {
+    ...(entryPath && { entryPath }),
+    ...(distPath && { distPath }),
+    ...(assetsPath && { assetsPath }),
+    ...(bundlePrefix && { bundlePrefix }),
+  };
+
+  //  construct metro options from cmd-line options
+  const { id, platform, dev } = options as MetroBundleOptions;
+  const bundleOptions: MetroBundleOptions = {
+    ...(id && { id }),
+    ...(platform && { platform }),
+    dev,
+  };
+
+  metroBundle(bundleConfig, bundleOptions, bundleOverrides);
+}
+
+export function rnxStart(
+  argv: Array<string>,
+  config /*: ConfigT*/,
+  options: Object
+): void {
+  //  unused params
+  argv;
+  config;
+
+  metroStart(options as MetroStartOptions);
+}
