e1fed87282369a16c1e0947f9a001b2a5550525f
Adam Foxman <afoxman@microsoft.com>
2021-03-09 12:50:59 -0800

    Add a postinstall step which automatically builds the build-tools.

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,3 +1,18 @@
-const { getPackageInfo } = require('just-repo-utils');
+const { spawnSync } = require("child_process");
+const os = require("os");
+
+const chalk = require("chalk");
+const { getPackageInfo } = require("just-repo-utils");
+
+function yarnSync(args) {
+  const yarnCommand = os.platform() === "win32" ? "yarn.cmd" : "yarn";
+  const spawnOptions = { cwd: process.cwd(), stdio: "inherit" };
+  spawnSync(yarnCommand, args, spawnOptions);
+}
+
 // force the cache to rebuild and reset
-getPackageInfo({ strategy: 'update' });
+getPackageInfo({ strategy: "update" });
+
+// build the build-tools
+console.log(chalk.cyan("\nBuilding the build system...\n"));
+yarnSync(["run", "build-tools"]);
