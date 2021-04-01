c0403db2cd27cc32fff48a17e1c1e18cf17809fd
Adam Foxman <adam@thefoxmans.net>
2021-04-01 05:29:59 -0700

    Pass-thru all jest args. Cleanup after dependabot merge. (#98)

--- scripts/src/tasks/jest.ts
+++ scripts/src/tasks/jest.ts
@@ -1,14 +1,24 @@
 import { argv, jestTask, JestTaskOptions } from "just-scripts";
 
 function getJestOptions(): JestTaskOptions {
-  const updateSnapshot =
-    argv().u || argv().updateSnapshot ? { updateSnapshot: true } : undefined;
+  //  pass through everything but the "test" / "jest" task name
+  const args = argv();
+  for (const element of ["jest", "test"]) {
+    const index = args._.indexOf(element);
+    if (index !== -1) {
+      args._.splice(index, 1);
+    }
+    if (args.hasOwnProperty(element)) {
+      delete args[element];
+    }
+  }
+
   return {
     config: "jest.config.js",
     coverage: !!argv().production,
     runInBand: true,
     passWithNoTests: true,
-    ...updateSnapshot,
+    ...args,
   };
 }
 
