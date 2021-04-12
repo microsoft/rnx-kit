dfb56e105d32f597b82468e41c795246251e568d
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-04-12 14:39:17 +0200

    feat: introduce `metro-serializer` for plugin support (#115)
    This new package enables plugin support in Metro. It does the exact same
    thing Metro does when serialising the JS bundle, but after running
    through specified plugins.
    
    Example usage:
    
    ```js
    const { makeMetroConfig } = require("@rnx-kit/metro-config");
    const {
      CyclicDependencies,
    } = require("@rnx-kit/metro-plugin-cyclic-dependencies-detector");
    const { MetroSerializer } = require("@rnx-kit/metro-serializer");
    
    module.exports = makeMetroConfig({
      projectRoot: __dirname,
      serializer: {
        customSerializer: MetroSerializer([
          CyclicDependencies(),
        ]),
      },
    });
    ```

---
+++ packages/metro-serializer/src/index.ts
@@ -0,0 +1,38 @@
+import type {
+  Graph,
+  MetroPlugin,
+  Module,
+  Serializer,
+  SerializerOptions,
+} from "./types";
+import * as semver from "semver";
+
+export * from "./types";
+
+/**
+ * Metro's default bundle serializer.
+ *
+ * Note that the return type changed to a `Promise` in
+ * [0.60](https://github.com/facebook/metro/commit/d6b9685c730d0d63577db40f41369157f28dfa3a).
+ *
+ * @see https://github.com/facebook/metro/blob/af23a1b27bcaaff2e43cb795744b003e145e78dd/packages/metro/src/Server.js#L228
+ */
+export function MetroSerializer(plugins: MetroPlugin[]): Serializer {
+  const baseJSBundle = require("metro/src/DeltaBundler/Serializers/baseJSBundle");
+  const bundleToString = require("metro/src/lib/bundleToString");
+
+  const { version } = require("metro/package.json");
+  const shouldReturnPromise = semver.satisfies(version, ">=0.60.0");
+
+  return (
+    entryPoint: string,
+    preModules: ReadonlyArray<Module>,
+    graph: Graph,
+    options: SerializerOptions
+  ): string | Promise<string> => {
+    plugins.forEach((plugin) => plugin(entryPoint, preModules, graph, options));
+    const bundle = baseJSBundle(entryPoint, preModules, graph, options);
+    const bundleCode = bundleToString(bundle).code;
+    return shouldReturnPromise ? Promise.resolve(bundleCode) : bundleCode;
+  };
+}
---
+++ packages/metro-serializer/test/index.test.ts
@@ -0,0 +1,59 @@
+import { MetroSerializer } from "../src/index";
+import type { BundleCode, Graph, SerializerOptions } from "../src/types";
+
+jest.mock("metro/src/DeltaBundler/Serializers/baseJSBundle");
+jest.mock("metro/src/lib/bundleToString");
+
+function isPromise<T>(obj: T | Promise<T>): obj is Promise<T> {
+  return (
+    Object.prototype.hasOwnProperty.call(obj, "then") &&
+    typeof obj["then"] === "function"
+  );
+}
+
+async function getBundleCode(
+  bundle: BundleCode | Promise<BundleCode>
+): Promise<string> {
+  if (isPromise(bundle)) {
+    return getBundleCode(await bundle);
+  }
+
+  if (typeof bundle === "string") {
+    return bundle;
+  }
+
+  throw new Error(`Expected 'string', got '${typeof bundle}'`);
+}
+
+describe("MetroSerializer", () => {
+  const baseJSBundle = require("metro/src/DeltaBundler/Serializers/baseJSBundle");
+  baseJSBundle.mockImplementation(() => undefined);
+
+  const bundleToString = require("metro/src/lib/bundleToString");
+  bundleToString.mockImplementation(() => ({
+    code: "code",
+    map: undefined,
+  }));
+
+  const mockGraph = {} as Graph;
+  const mockOptions = {} as SerializerOptions;
+
+  test("works without plugins", async () => {
+    const serializer = MetroSerializer([]);
+    const result = serializer("entryPoint", [], mockGraph, mockOptions);
+    expect(await getBundleCode(result)).toBe("code");
+  });
+
+  test("calls plugins in specified order", async () => {
+    const callOrder = [];
+    const serializer = MetroSerializer([
+      () => callOrder.push(1),
+      () => callOrder.push(2),
+      () => callOrder.push(3),
+    ]);
+    const result = serializer("entryPoint", [], mockGraph, mockOptions);
+    expect(callOrder).toEqual(callOrder.sort());
+
+    expect(await getBundleCode(result)).toBe("code");
+  });
+});
