584d92e28c6d5d4d8efc96172374bbbc70c816bd
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-04-12 19:07:59 +0200

    feat: cyclic dependencies plugin for Metro (#116)
    Metro plugin for detecting cyclic dependencies. To use it, add it to
    `metro-serializer` introduced in #115:
    
    ```js
    const { makeMetroConfig } = require("@rnx-kit/metro-config");
    const {
      CyclicDependencies,
    } = require("@rnx-kit/metro-plugin-cyclic-dependencies-detector");
    const { MetroSerializer } = require("@rnx-kit/metro-serializer");
    
    module.exports = makeMetroConfig({
      projectRoot: __dirname,
      serializer: {
        customSerializer: MetroSerializer([
          CyclicDependencies(),
        ]),
      },
    });
    ```
    
    Example output:
    
    ```
    @rnx-kit/test-app/lib/src/App.js
    └── @rnx-kit/test-app/lib/src/cyclicExample.js
        └── @rnx-kit/test-app/lib/src/App.js
            └── @rnx-kit/test-app/lib/src/index.js
    
    error Require cycles detected. Run CLI with --verbose flag for more details.
    Error: Require cycles detected
        at Object.detectCycles (/~/rnx-kit/packages/metro-plugin-cyclic-dependencies-detector/lib/detectCycles.js:82:15)
        at /~/rnx-kit/packages/metro-plugin-cyclic-dependencies-detector/lib/index.js:7:24
        at /~/rnx-kit/packages/metro-serializer/lib/index.js:42:37
        at Array.forEach (<anonymous>)
        at Object.customSerializer (/~/rnx-kit/packages/metro-serializer/lib/index.js:42:17)
        at /~/rnx-kit/node_modules/metro/src/Server.js:562:50
        at Generator.next (<anonymous>)
        at asyncGeneratorStep (/~/rnx-kit/node_modules/metro/src/Server.js:99:24)
        at _next (/~/rnx-kit/node_modules/metro/src/Server.js:119:9)
        at processTicksAndRejections (internal/process/task_queues.js:97:5)
    error Command failed with exit code 1.
    info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
    ```

---
+++ packages/metro-plugin-cyclic-dependencies-detector/src/detectCycles.ts
@@ -0,0 +1,110 @@
+import * as path from "path";
+import type { Dependencies, Graph } from "@rnx-kit/metro-serializer";
+
+export type CyclicDependencies = Record<string, string[]>;
+
+export type PluginOptions = {
+  includeNodeModules?: boolean;
+  linesOfContext?: number;
+  throwOnError?: boolean;
+};
+
+export function packageRelativePath(
+  modulePath: string,
+  cache: Record<string, string>
+): string {
+  const cachedPath = cache[modulePath];
+  if (cachedPath) {
+    return cachedPath;
+  }
+
+  const pkgDir = require("pkg-dir");
+
+  const packagePath = pkgDir.sync(modulePath);
+  const { name } = require(path.join(packagePath, "package.json"));
+  const relativePath = `${name}/${path.relative(packagePath, modulePath)}`;
+  cache[modulePath] = relativePath;
+  return relativePath;
+}
+
+export function traverseDependencies(
+  currentModule: string,
+  dependencies: Dependencies,
+  options: PluginOptions,
+  cyclicDependencies: CyclicDependencies = {},
+  stack: string[] = []
+): CyclicDependencies {
+  if (!options.includeNodeModules && currentModule.includes("/node_modules/")) {
+    return cyclicDependencies;
+  }
+
+  if (stack.includes(currentModule)) {
+    cyclicDependencies[currentModule] = stack.slice();
+    return cyclicDependencies;
+  }
+
+  stack.push(currentModule);
+
+  dependencies.get(currentModule)?.dependencies?.forEach((dependency) => {
+    if (dependency["__rnxCyclicDependenciesChecked"]) {
+      return;
+    }
+
+    traverseDependencies(
+      dependency.absolutePath,
+      dependencies,
+      options,
+      cyclicDependencies,
+      stack
+    );
+
+    // Performance optimization: There is no need to traverse this module again.
+    dependency["__rnxCyclicDependenciesChecked"] = true;
+  });
+
+  stack.pop();
+  return cyclicDependencies;
+}
+
+export function detectCycles(
+  entryPoint: string,
+  { dependencies }: Graph,
+  options: PluginOptions
+): void {
+  const cyclicDependencies = traverseDependencies(
+    entryPoint,
+    dependencies,
+    options
+  );
+
+  const modulePaths = Object.keys(cyclicDependencies);
+  if (modulePaths.length === 0) {
+    return;
+  }
+
+  const { linesOfContext = 1, throwOnError = true } = options;
+  const cachedPaths: Record<string, string> = {};
+
+  modulePaths.forEach((modulePath) => {
+    const currentModule = packageRelativePath(modulePath, cachedPaths);
+    console.warn(currentModule);
+
+    const requirePath = cyclicDependencies[modulePath];
+    const start = Math.max(requirePath.indexOf(modulePath) - linesOfContext, 0);
+    requirePath
+      .slice(start)
+      .reverse()
+      .forEach((module, index) => {
+        const requiredBy = packageRelativePath(module, cachedPaths);
+        console.warn(`${"    ".repeat(index)}└── ${requiredBy}`);
+      });
+    console.log();
+  });
+
+  if (throwOnError) {
+    throw new Error("Import cycles detected");
+  } else {
+    console.error("❌ Import cycles detected!");
+    console.log();
+  }
+}
