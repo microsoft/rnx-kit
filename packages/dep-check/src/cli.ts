1f697f4a4ca19ee376fd41f6f973bc2cef2b9be9
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-04-28 17:59:55 +0200

    fix(dep-check): expose cli for cli related integrations (#164)

--- packages/dep-check/src/cli.ts
+++ packages/dep-check/src/cli.ts
@@ -1,37 +1,47 @@
+#!/usr/bin/env node
 import path from "path";
 import pkgDir from "pkg-dir";
 import yargs from "yargs";
 import { checkPackageManifest } from "./check";
 
-yargs.usage(
-  "$0 [package-json]",
-  "",
-  {
-    write: {
-      default: false,
-      description: "Writes changes to the specified 'package.json'.",
-      type: "boolean",
-    },
-  },
-  ({ write, "package-json": packageJson }) => {
-    const packageManifest = (() => {
-      if (typeof packageJson === "string") {
-        return packageJson;
-      }
+export type Args = {
+  write: boolean;
+  "package-json"?: string | number;
+};
 
-      const packageDir = pkgDir.sync();
-      if (packageDir) {
-        return path.join(packageDir, "package.json");
-      }
-
-      return undefined;
-    })();
-    if (!packageManifest) {
-      console.error("Could not find package manifest");
-      process.exit(1);
+export function cli({ write, "package-json": packageJson }: Args) {
+  const packageManifest = (() => {
+    if (typeof packageJson === "string") {
+      return packageJson;
     }
 
-    const exitCode = checkPackageManifest(packageManifest, { write });
-    process.exit(exitCode);
+    const packageDir = pkgDir.sync();
+    if (packageDir) {
+      return path.join(packageDir, "package.json");
+    }
+
+    return undefined;
+  })();
+  if (!packageManifest) {
+    console.error("Could not find package manifest");
+    process.exit(1);
   }
-).argv;
+
+  const exitCode = checkPackageManifest(packageManifest, { write });
+  process.exit(exitCode);
+}
+
+if (require.main === module) {
+  yargs.usage(
+    "$0 [package-json]",
+    "Dependency checker for React Native apps",
+    {
+      write: {
+        default: false,
+        description: "Writes changes to the specified 'package.json'.",
+        type: "boolean",
+      },
+    },
+    cli
+  ).argv;
+}
--- packages/dep-check/src/index.ts
+++ packages/dep-check/src/index.ts
@@ -1,3 +1,4 @@
 export { checkPackageManifest } from "./check";
+export { Args, cli } from "./cli";
 export { updatePackageManifest } from "./manifest";
 export * from "./types";
