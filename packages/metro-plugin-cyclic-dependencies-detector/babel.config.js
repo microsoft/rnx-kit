43a53e8bc2aaa0df3fd9e957aa9abed82b906e73
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-05-04 14:32:00 +0200

    chore: extract Babel preset for Jest + TypeScript

--- packages/metro-plugin-cyclic-dependencies-detector/babel.config.js
+++ packages/metro-plugin-cyclic-dependencies-detector/babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-  ],
+  presets: ["@rnx-kit/babel-preset-jest-typescript"],
 };
