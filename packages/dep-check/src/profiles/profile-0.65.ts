c20ff4cdd6e961c0a697fba52b61bf4d5f9aa1c6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-05-05 22:55:34 +0200

    fix(dep-check): add preliminary profile for 0.65 (#191)

--- packages/dep-check/src/profiles/profile-0.65.ts
+++ packages/dep-check/src/profiles/profile-0.65.ts
@@ -1,34 +1,30 @@
 import type { Profile } from "../types";
-import profile61 from "./profile-0.61";
+import profile_0_64 from "./profile-0.64";
 
 const reactNative = {
   name: "react-native",
-  version: "^0.62.3",
+  version: "^0.65.0-0",
 };
 
 const profile: Profile = {
-  ...profile61,
-  react: {
-    name: "react",
-    version: "16.11.0",
-  },
+  ...profile_0_64,
   "core-android": reactNative,
   "core-ios": reactNative,
   "core-macos": {
     name: "react-native-macos",
-    version: "^0.62.0",
+    version: "^0.65.0-0",
   },
   "core-win32": {
     name: "@office-iss/react-native-win32",
-    version: "^0.62.0",
+    version: "^0.65.0-0",
   },
   "core-windows": {
     name: "react-native-windows",
-    version: "^0.62.0",
+    version: "^0.65.0-0",
   },
-  hermes: {
-    name: "hermes-engine",
-    version: "~0.4.0",
+  react: {
+    name: "react",
+    version: "17.0.2",
   },
 };
 
