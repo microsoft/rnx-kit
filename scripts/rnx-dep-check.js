c2f23e0f551466915ae521d32bdcd9beb00a1ff1
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-06-11 21:02:46 +0200

    chore: bump lage and use rnx-dep-check to align dependencies

---
+++ scripts/rnx-dep-check.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const common = {
+  chalk: {
+    name: "chalk",
+    version: "^4.1.0",
+  },
+  "find-up": {
+    name: "find-up",
+    version: "^5.0.0",
+  },
+  "jest-diff": {
+    name: "jest-diff",
+    version: "^26.0.0",
+  },
+  semver: {
+    name: "semver",
+    version: "^7.0.0",
+  },
+  typescript: {
+    name: "typescript",
+    version: "^4.0.0",
+  },
+  "workspace-tools": {
+    name: "workspace-tools",
+    version: "^0.16.2",
+  },
+  yargs: {
+    name: "yargs",
+    version: "^16.0.0",
+  },
+};
+
+module.exports = {
+  0.63: common,
+  0.64: common,
+  0.65: common,
+};
+
+if (require.main === module) {
+  require("@rnx-kit/dep-check").cli({
+    "custom-profiles": __filename,
+    vigilant: "0.64",
+    write: process.argv.includes("--write"),
+  });
+}
