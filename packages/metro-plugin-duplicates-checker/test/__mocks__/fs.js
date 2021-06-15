fb3dedc03971fac637d4be92795c6629114e14a2
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-06-15 16:14:08 +0200

    chore: bump Metro to 0.66

--- packages/metro-plugin-duplicates-checker/test/__mocks__/fs.js
+++ packages/metro-plugin-duplicates-checker/test/__mocks__/fs.js
@@ -1,14 +1,29 @@
 const fs = jest.createMockFromModule("fs");
 const actualFs = jest.requireActual("fs");
 
-// Under normal circumstances, this extra copy of 'fbjs' should not be installed.
-const extraFbjs = "/packages/test-app/node_modules/fbjs/package.json";
+// Under normal circumstances, this extra copy of '@react-native/polyfills'
+// should not be installed.
+const extraPolyfills =
+  "/packages/test-app/node_modules/@react-native/polyfills";
 
 fs.readFileSync = (path, ...args) => {
-  if (path.replace(/\\/g, "/").endsWith(extraFbjs)) {
-    return JSON.stringify({ name: "fbjs", version: "3.0.0" });
+  if (path.replace(/\\/g, "/").includes(extraPolyfills)) {
+    return JSON.stringify({
+      name: "@react-native/polyfills",
+      version: "1.0.0",
+    });
   } else {
     return actualFs.readFileSync(path, ...args);
+  }
+};
+
+fs.realpathSync = actualFs.realpathSync;
+const actualRealpathSyncNative = actualFs.realpathSync.native;
+fs.realpathSync.native = (path, ...args) => {
+  if (path.replace(/\\/g, "/").endsWith(extraPolyfills)) {
+    return path;
+  } else {
+    return actualRealpathSyncNative(path, ...args);
   }
 };
 
