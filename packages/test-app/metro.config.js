472a62986a264f24b1249ba79e747a31737d9dd7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-06-16 21:25:16 +0200

    fix(dep-check): bump react-native 0.64.1 -> 0.64.2

--- packages/test-app/metro.config.js
+++ packages/test-app/metro.config.js
@@ -10,12 +10,21 @@ const {
   TypeScriptValidation,
 } = require("@rnx-kit/metro-plugin-typescript-validation");
 
+function isDev() {
+  const devFlag = process.argv.findIndex((arg) => arg === "--dev");
+  if (devFlag >= 0) {
+    return process.argv[devFlag + 1] === "true";
+  }
+
+  return false;
+}
+
 module.exports = makeMetroConfig({
   projectRoot: __dirname,
   serializer: {
     customSerializer: MetroSerializer([
       CyclicDependencies(),
-      DuplicateDependencies(),
+      DuplicateDependencies({ ignoredModules: isDev() ? ["react-is"] : [] }),
       TypeScriptValidation(),
     ]),
   },
