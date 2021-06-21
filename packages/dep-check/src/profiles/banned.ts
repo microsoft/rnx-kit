97e5ae4b804010fe2457075ff5c1969391097b61
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-06-21 13:39:04 +0200

    fix(dep-check): warn about renamed packages (#303)
    - `@react-native-community/async-storage` ->
      `@react-native-async-storage/async-storage`
    - `@react-native-community/masked-view` ->
      `@react-native-masked-view/masked-view`

--- packages/dep-check/src/profiles/banned.ts
+++ packages/dep-check/src/profiles/banned.ts
@@ -2,6 +2,18 @@ import type { ExcludedPackage } from "../types";
 
 const bannedPackages: ExcludedPackage[] = [
   {
+    name: "@react-native-community/async-storage",
+    version: "*",
+    reason:
+      "This package was renamed to '@react-native-async-storage/async-storage' in 1.13.0. The new package will be used in 0.64.",
+  },
+  {
+    name: "@react-native-community/masked-view",
+    version: "*",
+    reason:
+      "This package was renamed to '@react-native-masked-view/masked-view' in 0.2.0. The new package will be used in 0.64.",
+  },
+  {
     name: "react-native-linear-gradient",
     version: "*",
     reason:
