d2c832f866d64e595f8cceae5139bbbd033098b5
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-06-21 19:27:17 +0200

    fix(dep-check): promote @react-native-masked-view/masked-view over @react-native-community/masked-view (#306)

--- packages/dep-check/src/profiles/banned.ts
+++ packages/dep-check/src/profiles/banned.ts
@@ -5,13 +5,13 @@ const bannedPackages: ExcludedPackage[] = [
     name: "@react-native-community/async-storage",
     version: "*",
     reason:
-      "This package was renamed to '@react-native-async-storage/async-storage' in 1.13.0. The new package will be used in 0.64.",
+      "This package was renamed to '@react-native-async-storage/async-storage' in 1.13.0. The new package will be recommended in 0.64.",
   },
   {
     name: "@react-native-community/masked-view",
     version: "*",
     reason:
-      "This package was renamed to '@react-native-masked-view/masked-view' in 0.2.0. The new package will be used in 0.64.",
+      "This package was renamed to '@react-native-masked-view/masked-view' in 0.2.0. Please remove the old package and start using the new one.",
   },
   {
     name: "react-native-linear-gradient",
