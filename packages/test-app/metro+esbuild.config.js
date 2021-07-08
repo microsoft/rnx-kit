0cabb2d056f8e683982e3c7b0c4769d0fb120c44
Adam Foxman <afoxman@microsoft.com>
2021-07-08 10:25:07 -0700

    Add Metro config options to kit config, removing the need to manually control Metro config. Update test app.

--- packages/test-app/metro+esbuild.config.js
+++ packages/test-app/metro+esbuild.config.js
@@ -1,22 +1,9 @@
 const { makeMetroConfig } = require("@rnx-kit/metro-config");
 const {
-  CyclicDependencies,
-} = require("@rnx-kit/metro-plugin-cyclic-dependencies-detector");
-const {
-  DuplicateDependencies,
-} = require("@rnx-kit/metro-plugin-duplicates-checker");
-const {
-  MetroSerializer,
   esbuildTransformerConfig,
 } = require("@rnx-kit/metro-serializer-esbuild");
 
 module.exports = makeMetroConfig({
   projectRoot: __dirname,
-  serializer: {
-    customSerializer: MetroSerializer([
-      CyclicDependencies(),
-      DuplicateDependencies({ ignoredModules: ["react-is"] }),
-    ]),
-  },
   transformer: esbuildTransformerConfig,
 });
