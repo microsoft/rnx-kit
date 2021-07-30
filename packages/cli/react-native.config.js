6c1a34522d12ccdc58c99bb5f3d35c078c18bff6
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-07-30 19:38:36 +0200

    copy/pasta mistake
    Co-authored-by: Adam Foxman <adam@thefoxmans.net>

--- packages/cli/react-native.config.js
+++ packages/cli/react-native.config.js
@@ -151,7 +151,7 @@ module.exports = {
         {
           name: "--json",
           description: "Output license information as a JSON",
-          default: true,
+          default: false,
           parse: parseBoolean,
         },
         {
