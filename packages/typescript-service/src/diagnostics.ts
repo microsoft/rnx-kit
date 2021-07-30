890b0b034e6145c9222d252b54d468f76970a7ba
Adam Foxman <afoxman@microsoft.com>
2021-07-30 11:52:25 -0700

    Add a custom write function for redirecting diagnostic output.

--- packages/typescript-service/src/diagnostics.ts
+++ packages/typescript-service/src/diagnostics.ts
@@ -6,7 +6,9 @@ export type DiagnosticWriter = {
   print: (diagnostic: ts.Diagnostic | ts.Diagnostic[]) => void;
 };
 
-export function createDiagnosticWriter(): DiagnosticWriter {
+export function createDiagnosticWriter(
+  write?: (message: string) => void
+): DiagnosticWriter {
   const formatDiagnosticsHost: ts.FormatDiagnosticsHost = {
     getCurrentDirectory: ts.sys.getCurrentDirectory,
     getCanonicalFileName,
@@ -23,7 +25,12 @@ export function createDiagnosticWriter(): DiagnosticWriter {
   }
 
   function print(diagnostic: ts.Diagnostic | ts.Diagnostic[]) {
-    ts.sys.write(format(diagnostic));
+    const message = format(diagnostic);
+    if (write) {
+      write(message);
+    } else {
+      ts.sys.write(message);
+    }
   }
 
   return {
