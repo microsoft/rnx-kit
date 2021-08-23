32ae291705cd20962bf4f315972d74ea088b7455
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-08-23 10:45:19 +0200

    fix(cli): `--experimental-tree-shake` shouldn't need extra config (#506)

--- packages/test-app/metro+esbuild.config.js
+++
@@ -1,9 +0,0 @@
-const { makeMetroConfig } = require("@rnx-kit/metro-config");
-const {
-  esbuildTransformerConfig,
-} = require("@rnx-kit/metro-serializer-esbuild");
-
-module.exports = makeMetroConfig({
-  projectRoot: __dirname,
-  transformer: esbuildTransformerConfig,
-});
