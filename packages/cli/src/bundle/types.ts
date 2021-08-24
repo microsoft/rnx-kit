ded70e4f73b0e855cd8417054c9ecc66c814907c
Adam Foxman <afoxman@microsoft.com>
2021-08-24 16:12:53 -0700

    Create a new type to encapsulate everything needed for bundling. Use this as the main type for driving metro bundle runs in the rnxBundle loop.

--- packages/cli/src/bundle/types.ts
+++ packages/cli/src/bundle/types.ts
@@ -6,5 +6,23 @@ import type { AllPlatforms } from "@rnx-kit/tools-react-native/platform";
 
 export type KitBundleConfig = BundleParameters &
   BundleRequiredParameters & {
+    /**
+     * Target platform for the bundle
+     */
     platform: AllPlatforms;
   };
+
+export type BundleConfig = KitBundleConfig & {
+  /**
+   * Choose whether or not this will be a "developer" bundle. The alternative is a "production" bundle.
+   *
+   * When `true`, warnings are enabled, and the bundle is not minified by default.
+   * When `false`, Warnings are disabled and the bundle is minified by default.
+   */
+  dev: boolean;
+
+  /**
+   * Optionally choose whether or not the bundle is minified. When not set, minification is controlled by the `dev` property.
+   */
+  minify?: boolean;
+};
