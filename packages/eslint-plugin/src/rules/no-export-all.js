d58ed2190321edbc823ec75e37374485f99e9d14
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-11-02 15:23:26 +0100

    TypeScript modules cannot be resolved with require.resolve

--- packages/eslint-plugin/src/rules/no-export-all.js
+++ packages/eslint-plugin/src/rules/no-export-all.js
@@ -9,6 +9,19 @@
 const fs = require("fs");
 const path = require("path");
 
+const DEFAULT_CONFIG = {
+  ecmaVersion: 9,
+  ecmaFeatures: { globalReturn: false, jsx: true },
+  sourceType: "module",
+  loc: true,
+  range: true,
+  raw: true,
+  tokens: true,
+  comment: true,
+  eslintVisitorKeys: true,
+  eslintScopeManager: true,
+};
+
 /**
  * Returns whether there are any named exports.
  * @param {NamedExports?} namedExports
@@ -22,26 +35,64 @@ function isEmpty(namedExports) {
 }
 
 /**
+ * Creates and returns an ES tree traverser.
+ * @returns {{ break: () => void; traverse: (node: Node, options: {}) => void}}
+ */
+function makeTraverser() {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const Traverser = require(path.join(
+    path.dirname(require.resolve("eslint/package.json")),
+    "lib",
+    "shared",
+    "traverser"
+  ));
+  return new Traverser();
+}
+
+/**
+ * Resolves specified `moduleId` relative to `fromDir`.
+ * @param {string} fromDir
+ * @param {string} moduleId
+ * @returns {string}
+ */
+function resolveFrom(fromDir, moduleId) {
+  if (process.env.NODE_ENV === "test") {
+    return moduleId;
+  }
+
+  if (moduleId.startsWith(".")) {
+    const p = path.join(fromDir, moduleId);
+    const ext = [".ts", ".tsx", ".js", ".jsx"].find((ext) =>
+      fs.existsSync(p + ext)
+    );
+    return p + ext;
+  }
+
+  return require.resolve(moduleId, { paths: [fromDir] });
+}
+
+/**
  * Parses specified file and returns an AST.
  * @param {import("eslint").Rule.RuleContext} context
- * @param {string} moduleName
+ * @param {string} moduleId
  * @returns {Node | null}
  */
-function parse(context, moduleName) {
-  const { parse: esParse } = require(context.parserPath);
-  if (typeof esParse !== "function") {
+function parse(context, moduleId) {
+  const { parseForESLint } = require(context.parserPath);
+  if (typeof parseForESLint !== "function") {
     return null;
   }
 
   try {
-    const p =
-      process.env.NODE_ENV === "test"
-        ? moduleName
-        : require.resolve(moduleName, {
-            paths: [path.dirname(context.getFilename())],
-          });
-    const code = fs.readFileSync(p, { encoding: "utf-8" });
-    return esParse(code, context.parserOptions);
+    const parentDir = path.dirname(context.getFilename());
+    const filePath = resolveFrom(parentDir, moduleId);
+    const code = fs.readFileSync(filePath, { encoding: "utf-8" });
+    return parseForESLint(code, {
+      ...DEFAULT_CONFIG,
+      ...context.parserOptions,
+      filePath,
+    }).ast;
   } catch (_) {
     /* ignore */
   }
@@ -52,15 +103,15 @@ function parse(context, moduleName) {
 /**
  * Extracts exports from specified file.
  * @param {import("eslint").Rule.RuleContext} context
- * @param {unknown} moduleName
+ * @param {unknown} moduleId
  * @returns {NamedExports | null}
  */
-function extractExports(context, moduleName) {
-  if (typeof moduleName !== "string") {
+function extractExports(context, moduleId) {
+  if (typeof moduleId !== "string") {
     return null;
   }
 
-  const ast = parse(context, moduleName);
+  const ast = parse(context, moduleId);
   if (!ast) {
     return null;
   }
@@ -68,11 +119,7 @@ function extractExports(context, moduleName) {
   try {
     /** @type {NamedExports} */
     const result = { exports: [], types: [] };
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const Traverser = require("eslint/lib/shared/traverser");
-    const traverser = new Traverser();
+    const traverser = makeTraverser();
     traverser.traverse(ast, {
       /** @type {(node: Node, parent: Node) => void} */
       enter: (node, _parent) => {
