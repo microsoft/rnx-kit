acebdc3d387fc38e11b487736e1d7ba065131f1d
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-11-03 19:09:13 +0100

    feat(eslint-plugin): implemented fixer for no-export-all (#795)

--- packages/eslint-plugin/src/rules/no-export-all.js
+++ packages/eslint-plugin/src/rules/no-export-all.js
@@ -1,6 +1,236 @@
 // @ts-check
 "use strict";
 
+/**
+ * @typedef {import("@typescript-eslint/types").TSESTree.Node} Node
+ * @typedef {import("eslint").Rule.RuleContext} ESLintRuleContext
+ * @typedef {{ exports: string[], types: string[] }} NamedExports
+ *
+ * @typedef {{
+ *   id: ESLintRuleContext["id"];
+ *   options: ESLintRuleContext["options"];
+ *   settings: ESLintRuleContext["settings"];
+ *   parserPath: ESLintRuleContext["parserPath"];
+ *   parserOptions: ESLintRuleContext["parserOptions"];
+ *   parserServices: ESLintRuleContext["parserServices"];
+ *   filename: string;
+ * }} RuleContext
+ */
+
+const fs = require("fs");
+const path = require("path");
+
+const DEFAULT_CONFIG = {
+  ecmaVersion: 9,
+  ecmaFeatures: { globalReturn: false, jsx: true },
+  sourceType: "module",
+  loc: true,
+  range: true,
+  raw: true,
+  tokens: true,
+  comment: true,
+  eslintVisitorKeys: true,
+  eslintScopeManager: true,
+};
+
+const MAX_DEPTH = 5;
+
+/**
+ * Returns whether there are any named exports.
+ * @param {NamedExports?} namedExports
+ * @returns {namedExports is null}
+ */
+function isEmpty(namedExports) {
+  return (
+    !namedExports ||
+    (namedExports.exports.length === 0 && namedExports.types.length === 0)
+  );
+}
+
+/**
+ * Creates and returns an ES tree traverser.
+ * @returns {{ traverse: (node: Node, options: {}) => void; }}
+ */
+function makeTraverser() {
+  const Traverser = require(path.join(
+    path.dirname(require.resolve("eslint/package.json")),
+    "lib",
+    "shared",
+    "traverser"
+  ));
+  return new Traverser();
+}
+
+/**
+ * Resolves specified `moduleId` starting from `fromDir`.
+ */
+const resolveFrom =
+  /** @type {() => (fromDir: string, moduleId: string) => string} */
+  (
+    () => {
+      if (process.env.NODE_ENV === "test") {
+        return (_, moduleId) => moduleId;
+      }
+
+      const resolve = require("enhanced-resolve").create.sync({
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+      });
+
+      return (fromDir, moduleId) => {
+        const m = resolve(fromDir, moduleId);
+        if (!m) {
+          throw new Error(
+            `Module not found: ${moduleId} (start path: ${fromDir})`
+          );
+        }
+        return m;
+      };
+    }
+  )();
+
+/**
+ * Converts ESLint's `RuleContext` to our `RuleContext`.
+ * @param {ESLintRuleContext} context
+ * @returns {RuleContext}
+ */
+function toRuleContext(context) {
+  return {
+    id: context.id,
+    options: context.options,
+    settings: context.settings,
+    parserPath: context.parserPath,
+    parserOptions: context.parserOptions,
+    parserServices: context.parserServices,
+    filename: context.getFilename(),
+  };
+}
+
+/**
+ * Parses specified file and returns an AST.
+ * @param {RuleContext} context
+ * @param {string} moduleId
+ * @returns {{ ast: Node; filename: string; } | null}
+ */
+function parse({ filename, parserPath, parserOptions }, moduleId) {
+  const { parseForESLint } = require(parserPath);
+  if (typeof parseForESLint !== "function") {
+    return null;
+  }
+
+  try {
+    const parentDir = path.dirname(filename);
+    const modulePath = resolveFrom(parentDir, moduleId);
+    const code = fs.readFileSync(modulePath, { encoding: "utf-8" });
+    return {
+      ast: parseForESLint(code, {
+        ...DEFAULT_CONFIG,
+        ...parserOptions,
+        filePath: modulePath,
+      }).ast,
+      filename: modulePath,
+    };
+  } catch (_) {
+    /* ignore */
+  }
+
+  return null;
+}
+
+/**
+ * Extracts exports from specified file.
+ * @param {RuleContext} context
+ * @param {unknown} moduleId
+ * @param {number=} depth
+ * @returns {NamedExports | null}
+ */
+function extractExports(context, moduleId, depth = 0) {
+  if (depth >= MAX_DEPTH || typeof moduleId !== "string") {
+    return null;
+  }
+
+  const parseResult = parse(context, moduleId);
+  if (!parseResult?.ast) {
+    return null;
+  }
+
+  const { ast, filename } = parseResult;
+  try {
+    /** @type {NamedExports} */
+    const result = { exports: [], types: [] };
+    const traverser = makeTraverser();
+    traverser.traverse(ast, {
+      /** @type {(node: Node, parent: Node) => void} */
+      enter: (node, _parent) => {
+        switch (node.type) {
+          case "ExportNamedDeclaration":
+            if (node.declaration) {
+              switch (node.declaration.type) {
+                case "ClassDeclaration":
+                // fallthrough
+                case "FunctionDeclaration": {
+                  const name = node.declaration.id?.name;
+                  if (name) {
+                    result.exports.push(name);
+                  }
+                  break;
+                }
+
+                case "TSInterfaceDeclaration":
+                // fallthrough
+                case "TSTypeAliasDeclaration": {
+                  const name = node.declaration.id?.name;
+                  if (name) {
+                    result.types.push(name);
+                  }
+                  break;
+                }
+
+                case "VariableDeclaration":
+                  node.declaration.declarations.forEach((declaration) => {
+                    if (declaration.id.type === "Identifier") {
+                      result.exports.push(declaration.id.name);
+                    }
+                  });
+                  break;
+              }
+            } else {
+              const list =
+                node.exportKind === "type" ? result.types : result.exports;
+              node.specifiers.forEach((spec) => {
+                const name = spec.exported.name;
+                if (name !== "default") {
+                  list.push(name);
+                }
+              });
+            }
+            break;
+
+          case "ExportAllDeclaration": {
+            const source = node.source?.value;
+            if (source) {
+              const namedExports = extractExports(
+                { ...context, filename },
+                source,
+                depth + 1
+              );
+              if (namedExports) {
+                result.exports.push(...namedExports.exports);
+                result.types.push(...namedExports.types);
+              }
+            }
+            break;
+          }
+        }
+      },
+    });
+    return result;
+  } catch (_) {
+    /* ignore */
+  }
+
+  return null;
+}
+
 /** @type {import("eslint").Rule.RuleModule} */
 module.exports = {
   meta: {
@@ -17,10 +247,31 @@ module.exports = {
   create: (context) => {
     return {
       ExportAllDeclaration: (node) => {
+        const result = extractExports(
+          toRuleContext(context),
+          node.source.value
+        );
         context.report({
           node,
           message:
             "Prefer explicit exports over `export *` to avoid name clashes, and improve tree-shakeability.",
+          fix: isEmpty(result)
+            ? null
+            : (fixer) => {
+                /** @type {string[]} */
+                const lines = [];
+                if (result.exports.length > 0) {
+                  const names = result.exports.sort().join(", ");
+                  lines.push(`export { ${names} } from ${node.source.raw};`);
+                }
+                if (result.types.length > 0) {
+                  const types = result.types.sort().join(", ");
+                  lines.push(
+                    `export type { ${types} } from ${node.source.raw};`
+                  );
+                }
+                return fixer.replaceText(node, lines.join("\n"));
+              },
         });
       },
     };
--- packages/eslint-plugin/test/no-export-all.test.ts
+++ packages/eslint-plugin/test/no-export-all.test.ts
@@ -1,11 +1,45 @@
 import { RuleTester } from "eslint";
 import rule from "../src/rules/no-export-all";
 
+jest.mock("fs");
+
+require("fs").__setMocks({
+  barbarian: "export const name = 'Conan';",
+  chopper: `
+export type Predator = { kind: "$predator" };
+
+export interface IChopper {
+  kind: "$helicopter"
+};
+
+export class Chopper implements IChopper {};
+
+export const name = "Dutch";
+export function escape() {
+  console.log("Get to da choppah!");
+}
+
+export { escape as escapeRe, name as nameRe };
+export type { IChopper as IChopperRe, Predator as PredatorRe };
+`,
+  conquerer: "export * from 'destroyer'",
+  destroyer: "export * from 'barbarian'",
+  recall: "export * from 'recall'",
+  types: `
+export type Predator = { kind: "$predator" };
+
+export interface IChopper {
+  kind: "$helicopter"
+};
+`,
+});
+
 const config = {
   env: {
     es6: true,
     node: true,
   },
+  parser: require.resolve("@typescript-eslint/parser"),
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -30,6 +64,30 @@ describe("disallows `export *`", () => {
       {
         code: "export * from 'chopper';",
         errors: 1,
+        output: [
+          "export { Chopper, escape, escapeRe, name, nameRe } from 'chopper';",
+          "export type { IChopper, IChopperRe, Predator, PredatorRe } from 'chopper';",
+        ].join("\n"),
+      },
+      {
+        code: "export * from 'conquerer';",
+        errors: 1,
+        output: "export { name } from 'conquerer';",
+      },
+      {
+        code: "export * from 'recall';",
+        errors: 1,
+        output: "export * from 'recall';",
+      },
+      {
+        code: "export * from 'this-package-does-not-exist';",
+        errors: 1,
+        output: "export * from 'this-package-does-not-exist';",
+      },
+      {
+        code: "export * from 'types';",
+        errors: 1,
+        output: "export type { IChopper, Predator } from 'types';",
       },
     ],
   });
