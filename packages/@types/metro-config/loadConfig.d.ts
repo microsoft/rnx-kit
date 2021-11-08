9e559d04f9afc885f165b2266cd77fb2437f48bd
Adam Foxman <afoxman@microsoft.com>
2021-11-07 18:55:22 -0800

    Transition from using our private Metro @types packages to the officially published @types packages.

--- packages/@types/metro-config/loadConfig.d.ts
+++
@@ -1,6 +0,0 @@
-import type { YargArguments, InputConfigT, ConfigT } from "metro-config";
-
-export function loadConfig(
-  argv?: YargArguments = {},
-  defaultConfigOverrides?: InputConfigT = {}
-): Promise<ConfigT>;
--- packages/@types/metro-resolver/index.d.ts
+++
@@ -1,9 +0,0 @@
-export * from "./types";
-
-import type { ResolutionContext, Resolution } from "./types";
-
-export function resolve(
-  context: ResolutionContext,
-  moduleName: string,
-  platform: string | null
-): Resolution;
