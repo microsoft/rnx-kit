646b90d5771c866d78a9ffcb0375ce8dab018101
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2021-11-12 13:58:10 +0100

    fix(dep-check): fix dep-check not being executable (#842)

--- scripts/src/tasks/bundle.ts
+++ scripts/src/tasks/bundle.ts
@@ -8,6 +8,7 @@ export function bundle(): void {
   const manifest = fs.readFileSync("package.json", { encoding: "utf-8" });
   const { main, dependencies } = JSON.parse(manifest);
 
+  const targetPlatform = platform || "node";
   esbuild.build({
     bundle: true,
     entryPoints: ["src/index.ts"],
@@ -17,6 +18,8 @@ export function bundle(): void {
     ],
     minify,
     outfile: main,
-    platform: platform || "node",
+    platform: targetPlatform,
+    banner:
+      targetPlatform === "node" ? { js: "#!/usr/bin/env node" } : undefined,
   });
 }
