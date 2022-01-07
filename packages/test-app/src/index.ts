bc2a805dbaddba10eb320d0ecbc730af72f1bc8e
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-01-07 11:37:36 +0100

    fix(test-app): fix typings and bump dependencies

--- packages/test-app/src/index.ts
+++ packages/test-app/src/index.ts
@@ -1,10 +1,4 @@
-/**
- * @format
- */
-
 import { AppRegistry } from "react-native";
-// eslint-disable-next-line
-// @ts-ignore
 import App from "./App";
 import { name as appName } from "../app.json";
 
