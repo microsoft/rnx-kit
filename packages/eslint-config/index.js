1b5edd22b03cec53890a234711aa8665d086fbff
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-01-28 11:25:49 +0100

    chore: enable @microsoft/sdl/required ESLint rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,8 @@
 module.exports = {
-  extends: ["plugin:@rnx-kit/recommended"],
+  extends: [
+    "plugin:@microsoft/sdl/required",
+    "plugin:@rnx-kit/recommended",
+  ],
   rules: {
     "@rnx-kit/no-export-all": "error",
   },
