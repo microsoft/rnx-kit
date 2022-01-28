4be9dde59e1ffe03fd974091385ffd53659c3b16
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-01-28 11:37:18 +0100

    Update packages/eslint-config/index.js
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    "plugin:@microsoft/sdl/required",
-    "plugin:@rnx-kit/recommended",
-  ],
+  extends: ["plugin:@microsoft/sdl/required", "plugin:@rnx-kit/recommended"],
   rules: {
     "@rnx-kit/no-export-all": "error",
   },
