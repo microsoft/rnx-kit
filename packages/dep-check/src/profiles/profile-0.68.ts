4c747fd9e19504406bf96fd949cc26aaaff42f47
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-08 17:58:14 +0100

    fix(dep-check): add initial profile for react-native 0.68 (#1069)

---
+++ packages/dep-check/src/profiles/profile-0.68.ts
@@ -0,0 +1,81 @@
+import type { Profile, Package } from "../types";
+import profile_0_67 from "./profile-0.67";
+
+const reactNative: Package = {
+  name: "react-native",
+  version: "^0.68.0-0",
+  capabilities: ["react"],
+};
+
+const profile: Profile = {
+  ...profile_0_67,
+  react: {
+    name: "react",
+    version: "17.0.2",
+  },
+  "react-dom": {
+    name: "react-dom",
+    version: "17.0.2",
+    capabilities: ["react"],
+  },
+  "react-test-renderer": {
+    name: "react-test-renderer",
+    version: "17.0.2",
+    capabilities: ["react"],
+    devOnly: true,
+  },
+  core: reactNative,
+  "core-android": reactNative,
+  "core-ios": reactNative,
+  "core-macos": {
+    name: "react-native-macos",
+    version: "^0.68.0-0",
+    capabilities: ["react"],
+  },
+  "core-windows": {
+    name: "react-native-windows",
+    version: "^0.68.0-0",
+    capabilities: ["core"],
+  },
+  "babel-preset-react-native": {
+    name: "metro-react-native-babel-preset",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  hermes: {
+    name: "hermes-engine",
+    version: "~0.11.0",
+  },
+  metro: {
+    name: "metro",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  "metro-config": {
+    name: "metro-config",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  "metro-core": {
+    name: "metro-core",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  "metro-react-native-babel-transformer": {
+    name: "metro-react-native-babel-transformer",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  "metro-resolver": {
+    name: "metro-resolver",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+  "metro-runtime": {
+    name: "metro-runtime",
+    version: "^0.67.0",
+    devOnly: true,
+  },
+};
+
+export default profile;
