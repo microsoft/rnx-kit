5efaf772a51dbd3c615d078742f132b3b9eb3d5c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-15 10:51:11 +0100

    condition extraNodeModules on env variable

--- packages/test-app/metro.config.js
+++ packages/test-app/metro.config.js
@@ -1,26 +1,31 @@
 const { exclusionList, makeMetroConfig } = require("@rnx-kit/metro-config");
 const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 
+// If USE_AUTH_MOCK=1, exclude the real module to enable the mock.
+const useAuthMock = process.env["USE_AUTH_MOCK"];
+
 const blockList = exclusionList([
   // Metro will pick up react-native-macos/-windows mocks if we don't exclude them
   /.*__fixtures__.*/,
 
   // If USE_AUTH_MOCK=1, exclude the real module to enable the mock.
-  ...(process.env["USE_AUTH_MOCK"]
-    ? [/.*packages[/\\]react-native-auth.*/]
-    : []),
+  ...(useAuthMock ? [/.*packages[/\\]react-native-auth.*/] : []),
 ]);
 
 module.exports = makeMetroConfig({
   resolver: {
     extraNodeModules: {
-      "@rnx-kit/react-native-auth": require("path").join(
-        __dirname,
-        "test",
-        "__mocks__",
-        "@rnx-kit",
-        "react-native-auth.js"
-      ),
+      ...(useAuthMock
+        ? {
+            "@rnx-kit/react-native-auth": require("path").join(
+              __dirname,
+              "test",
+              "__mocks__",
+              "@rnx-kit",
+              "react-native-auth.js"
+            ),
+          }
+        : {}),
     },
     resolveRequest: MetroSymlinksResolver(),
     blacklistRE: blockList,
