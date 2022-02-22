95a43cb2e9bc60f165c894da59b2c0bdf5aa99c7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-02-22 17:26:15 +0100

    fix(eslint-plugin): enable `react-hooks/recommended`

--- packages/eslint-plugin/src/configs/recommended.js
+++ packages/eslint-plugin/src/configs/recommended.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:react/recommended",
   ],
   parser: "@typescript-eslint/parser",
