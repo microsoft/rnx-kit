daefe6de08485c5314788752b8db9a2592ea037d
Adam Foxman <afoxman@microsoft.com>
2022-02-27 15:08:06 -0800

    working docusaurus site. mostly stock with a few minor tweaks.

---
+++ docsite/docusaurus.config.js
@@ -0,0 +1,124 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+const organizationName = "microsoft";
+const projectName = "rnx-kit";
+const githubUrl = "https://github.com/" + organizationName + "/" + projectName;
+const mainBranchUrl = githubUrl + "/tree/main";
+const docsiteUrl = mainBranchUrl + "/docsite";
+
+const title = "React Native Developer Tools";
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title,
+  tagline:
+    "Tools which help developers build, deliver, and maintain React Native apps and libraries.",
+  url: "https://" + organizationName + ".github.io",
+  baseUrl: "/" + projectName + "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName,
+  projectName,
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: docsiteUrl + "/docs/",
+        },
+        blog: {
+          showReadingTime: true,
+          editUrl: docsiteUrl + "/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: projectName,
+        logo: {
+          alt: "react logo",
+          src: "img/react-logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: githubUrl,
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Tutorial",
+                to: "/docs/intro",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: githubUrl,
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Microsoft`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
