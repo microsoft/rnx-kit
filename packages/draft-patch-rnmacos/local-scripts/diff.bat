b3ce52ac42a8a7953b548ba80dd358bf4cab8074
Lorenzo Sciandra <lsciandra@microsoft.com>
2022-03-01 11:24:09 +0000

    delete entirely the local scripts folder

--- packages/draft-patch-rnmacos/local-scripts/diff.bat
+++
@@ -1,5 +0,0 @@
-REM node %~dp0\..\bundle\bundle.js diff %~dp0\..\.. E:\github\fb-rn-p --patch-name patches
-
-REM node %~dp0\..\bundle\bundle.js diff %~dp0\..\.. E:\github\fb-rn-p --patch-name DynamicFromObject --inclusionList-dirs ReactAndroid\src\main\java\com\facebook\react\bridge\
-
-node %~dp0\..\bundle\bundle.js diff %~dp0\..\.. E:\github\ms-react-native-minus --patch-name TextInputJS --inclusionList-dirs Libraries\Components\TextInput\
