db040e1ba393fdd6f77a2b1ff7f24c26c8cc2c22
Lorenzo Sciandra <lsciandra@microsoft.com>
2022-03-08 14:22:17 +0000

    modify logic in the script

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -7,6 +7,8 @@ const PACKAGE_JSON = "package.json";
 const README = "README.md";
 const TOKEN_START = "<!-- @rnx-kit start -->";
 const TOKEN_END = "<!-- @rnx-kit end -->";
+const TOKEN_EXPERIMENTAL_START = "<!-- @rnx-kit experimental start -->";
+const TOKEN_EXPERIMENTAL_END = "<!-- @rnx-kit experimental end -->";
 
 function readFile(path) {
   return fs.readFileSync(path, { encoding: "utf-8" });
@@ -18,15 +20,20 @@ const repoUrl = (() => {
 })();
 
 const repoRoot = getWorkspaceRoot();
+const experimentalPackages = [];
 const packages = getAllPackageJsonFiles(repoRoot).reduce(
   (packages, manifestPath) => {
     const content = readFile(manifestPath);
     const manifest = JSON.parse(content);
-    const { private, name, description } = manifest;
+    const { private, name, description, experimental } = manifest;
     if (!private && !name.startsWith("@types/")) {
       const packagePath = path.relative(repoRoot, path.dirname(manifestPath));
       const link = `[${name}](${repoUrl}/tree/main/${packagePath})`;
-      packages.push([link, description]);
+      if (experimental) {
+        experimentalPackages.push([link, description.substring(34)]);
+      } else {
+        packages.push([link, description]);
+      }
     }
     return packages;
   },
@@ -34,13 +41,23 @@ const packages = getAllPackageJsonFiles(repoRoot).reduce(
 );
 
 const table = markdownTable([["Name", "Description"], ...packages]);
+const experimentalTable = markdownTable([
+  ["Name", "Description"],
+  ...experimentalPackages,
+]);
 
 const readme = readFile(README);
-const updatedReadme = readme.replace(
+
+const updatedMainTableReadme = readme.replace(
   new RegExp(`${TOKEN_START}([^]+)${TOKEN_END}`),
   `${TOKEN_START}\n\n${table}\n\n${TOKEN_END}`
 );
 
-if (updatedReadme !== readme) {
-  fs.writeFileSync(README, updatedReadme);
+const fullyUpdatedReadme = updatedMainTableReadme.replace(
+  new RegExp(`${TOKEN_EXPERIMENTAL_START}([^]+)${TOKEN_EXPERIMENTAL_END}`),
+  `${TOKEN_EXPERIMENTAL_START}\n\n${experimentalTable}\n\n${TOKEN_EXPERIMENTAL_END}`
+);
+
+if (fullyUpdatedReadme !== readme) {
+  fs.writeFileSync(README, fullyUpdatedReadme);
 }
