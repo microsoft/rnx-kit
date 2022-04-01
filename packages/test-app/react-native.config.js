5620c7f3b4e645aaf24e34efabcfe35ad0e93121
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-04-01 16:04:29 +0200

    chore: update `react-native.config.js`

--- packages/test-app/react-native.config.js
+++ packages/test-app/react-native.config.js
@@ -1,61 +1,30 @@
-const fs = require("fs");
-const path = require("path");
-
-const windowsProjectFile = path.join(
-  "node_modules",
-  ".generated",
-  "windows",
-  "ReactTestApp",
-  "ReactTestApp.vcxproj"
-);
+const project = (() => {
+  const fs = require("fs");
+  const path = require("path");
+  try {
+    const {
+      androidManifestPath,
+      iosProjectPath,
+      windowsProjectPath,
+    } = require("react-native-test-app");
+    const iosProject = iosProjectPath("ios");
+    return {
+      android: {
+        sourceDir: "android",
+        manifestPath: androidManifestPath(path.join(__dirname, "android")),
+      },
+      windows: fs.existsSync("windows/Example.sln") && {
+        sourceDir: "windows",
+        solutionFile: "Example.sln",
+        project: windowsProjectPath(path.join(__dirname, "windows")),
+      },
+      ...(iosProject ? { ios: { project: iosProject } } : undefined),
+    };
+  } catch (_) {
+    return undefined;
+  }
+})();
 
 module.exports = {
-  project: {
-    android: {
-      sourceDir: "android",
-      manifestPath: path.relative(
-        path.join(__dirname, "android"),
-        path.join(
-          path.dirname(require.resolve("react-native-test-app/package.json")),
-          "android",
-          "app",
-          "src",
-          "main",
-          "AndroidManifest.xml"
-        )
-      ),
-    },
-    ios: {
-      project: (() => {
-        const {
-          packageSatisfiesVersionRange,
-        } = require("react-native-test-app/scripts/configure");
-        if (
-          packageSatisfiesVersionRange(
-            "@react-native-community/cli-platform-ios",
-            "<5.0.2"
-          )
-        ) {
-          // Prior to @react-native-community/cli-platform-ios v5.0.0,
-          // `project` was only used to infer `sourceDir` and `podfile`.
-          return "ios/ReactTestApp-Dummy.xcodeproj";
-        }
-
-        // `sourceDir` and `podfile` detection was fixed in
-        // @react-native-community/cli-platform-ios v5.0.2 (see
-        // https://github.com/react-native-community/cli/pull/1444).
-        return "node_modules/.generated/ios/ReactTestApp.xcodeproj";
-      })(),
-    },
-    windows: fs.existsSync(windowsProjectFile) && {
-      sourceDir: "windows",
-      solutionFile: "Example.sln",
-      project: {
-        projectFile: path.relative(
-          path.join(__dirname, "windows"),
-          windowsProjectFile
-        ),
-      },
-    },
-  },
+  ...(project ? { project } : undefined),
 };
