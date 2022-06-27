bdb9ed2b9cae4ebbf877ffae249ab3559d074d39
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-06-27 11:08:57 +0200

    rename time constants

--- incubator/build/src/time.ts
+++ incubator/build/src/time.ts
@@ -1,6 +1,6 @@
-const SECONDS = 1000;
-const MINUTES = 60 * SECONDS;
-const HOURS = 60 * MINUTES;
+const MS_PER_SECOND = 1000;
+const MS_PER_MINUTE = 60 * MS_PER_SECOND;
+const MS_PER_HOUR = 60 * MS_PER_MINUTE;
 
 const { now, parse: parseDate } = Date;
 const floor = Math.floor;
@@ -12,9 +12,9 @@ export function elapsedTime(
   const end = endTime ? parseDate(endTime) : now();
   const elapsed = end - parseDate(startTime);
 
-  const hours = floor(elapsed / HOURS);
-  const minutes = floor((elapsed % HOURS) / MINUTES);
-  const seconds = floor((elapsed % MINUTES) / SECONDS);
+  const hours = floor(elapsed / MS_PER_HOUR);
+  const minutes = floor((elapsed % MS_PER_HOUR) / MS_PER_MINUTE);
+  const seconds = floor((elapsed % MS_PER_MINUTE) / MS_PER_SECOND);
 
   if (hours > 0) {
     return `${hours}h ${minutes}m ${seconds}s`;
