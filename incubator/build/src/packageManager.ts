56e45de45f226d7af16bfbb0cd71df353023b78d
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-07-05 22:15:56 +0200

    fix types

--- incubator/build/src/packageManager.ts
+++ incubator/build/src/packageManager.ts
@@ -1,17 +1,18 @@
 import findUp from "find-up";
 import * as path from "node:path";
 
-type PackageManager = "npm" | "pnpm" | "yarn";
-
-const PACKAGE_MANAGERS: Record<string, PackageManager> = {
+const PACKAGE_MANAGERS = {
   "yarn.lock": "yarn",
   "package-lock.json": "npm",
   "pnpm-lock.yaml": "pnpm",
-};
+} as const;
+
+type LockFile = keyof typeof PACKAGE_MANAGERS;
+type PackageManager = typeof PACKAGE_MANAGERS[LockFile];
 
 export async function detectPackageManager(): Promise<
   PackageManager | undefined
 > {
-  const lockfile = await findUp(Object.keys(PACKAGE_MANAGERS));
-  return lockfile ? PACKAGE_MANAGERS[path.basename(lockfile)] : undefined;
+  const file = await findUp(Object.keys(PACKAGE_MANAGERS));
+  return file ? PACKAGE_MANAGERS[path.basename(file) as LockFile] : undefined;
 }
