fb757de10a8cf394e27b9067efef6b78c4ba6455
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-07-08 15:03:37 +0200

    fix(rnx-build): move config file into OS-specific path (#1730)

--- incubator/build/src/constants.ts
+++ incubator/build/src/constants.ts
@@ -1,9 +1,10 @@
-import * as os from "node:os";
+import envPaths from "env-paths";
 import * as path from "node:path";
 
 export const MAX_ATTEMPTS = 8;
 export const MAX_DOWNLOAD_ATTEMPTS = 3;
 
 export const BUILD_ID = "rnx-build";
-export const USER_CONFIG_FILE = path.join(os.homedir(), `.${BUILD_ID}.json`);
+export const CONFIG_DIR = envPaths(BUILD_ID).config;
+export const USER_CONFIG_FILE = path.join(CONFIG_DIR, "config.json");
 export const WORKFLOW_ID = BUILD_ID + ".yml";
