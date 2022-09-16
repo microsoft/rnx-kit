c98bcd70c42a423fedb9528d0f79a259d785e971
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-09-16 09:43:02 +0200

    split import type

--- packages/metro-service/src/ramBundle.ts
+++ packages/metro-service/src/ramBundle.ts
@@ -1,7 +1,8 @@
 import type { ConfigT } from "metro-config";
 // @ts-expect-error no declaration file for module
 import RamBundle from "metro/src/shared/output/RamBundle";
-import { bundle, BundleArgs } from "./bundle";
+import type { BundleArgs } from "./bundle";
+import { bundle } from "./bundle";
 
 export function ramBundle(args: BundleArgs, config: ConfigT): Promise<void> {
   return bundle(args, config, RamBundle);
