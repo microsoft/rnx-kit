786e24c7460129a7118121c30230d72b439f19ea
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2022-11-28 10:26:56 +0100

    docs: replace tsdoc with typedoc (#2048)

--- packages/tools-react-native/src/platform.ts
+++ packages/tools-react-native/src/platform.ts
@@ -31,7 +31,6 @@ export function expandPlatformExtensions(
 
 /**
  * Returns a map of available React Native platforms. The result is cached.
- * @privateRemarks is-arrow-function
  * @param startDir The directory to look for react-native platforms from
  * @returns A platform-to-npm-package map, excluding "core" platforms.
  */
