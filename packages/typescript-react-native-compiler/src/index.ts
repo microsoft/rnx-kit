47272030801cf453e3cbc2f44e600e8b0babf04e
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-01-11 13:00:33 +0100

    chore: remove deprecated typescript-react-native-compiler

--- packages/typescript-react-native-compiler/src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { CommandLine } from "./command-line";
-export { parseCommandLine } from "./command-line";
-export { createProgram, createIncrementalProgram } from "./program";
--- packages/typescript-react-native-compiler/test/__fixtures__/ts/f.native.ts
+++
@@ -1,3 +0,0 @@
-export function fnative(): string {
-  return "fnative";
-}
