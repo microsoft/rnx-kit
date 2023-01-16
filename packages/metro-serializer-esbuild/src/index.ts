b781fe38f5665c96119ba4c3de380e92cb40c8bd
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-01-16 14:17:26 +0100

    fix prelude

--- packages/metro-serializer-esbuild/src/index.ts
+++ packages/metro-serializer-esbuild/src/index.ts
@@ -164,6 +164,8 @@ export function MetroSerializer(
       return Promise.resolve(bundleCode);
     }
 
+    const prelude = "__rnx_prelude__";
+
     const { dependencies } = graph;
     const metroPlugin: Plugin = {
       name: require("../package.json").name,
@@ -210,7 +212,7 @@ export function MetroSerializer(
           }
 
           if (
-            args.path === __filename ||
+            args.path === prelude ||
             preModules.find(({ path }) => path === args.path)
           ) {
             return {
@@ -252,7 +254,7 @@ export function MetroSerializer(
             };
           }
 
-          if (args.path === __filename) {
+          if (args.path === prelude) {
             return {
               /**
                * Add all the polyfills in this file. See the `inject` option
@@ -340,7 +342,7 @@ export function MetroSerializer(
            * to medium sized app. We can work around this issue by adding all
            * the polyfills in a single file that we inject here.
            */
-          __filename,
+          prelude,
         ],
         legalComments: "none",
         logLevel: buildOptions?.logLevel ?? "error",
