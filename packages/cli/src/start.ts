8d646ec2f48f77d8d9e2b7ed94bfd6899b8ec287
John Ferlito <johnf@inodes.org>
2023-02-16 14:01:12 +1100

    feat(cli): Print rnx-start help again if *h* is pressed

--- packages/cli/src/start.ts
+++ packages/cli/src/start.ts
@@ -92,6 +92,20 @@ export async function rnxStart(
     cliOptions.customLogReporterPath
   );
 
+  const printHelp = () => {
+    const dim = chalk.dim;
+    const press = dim(" › Press ");
+    [
+      ["r", "reload the app"],
+      ["d", "open developer menu"],
+      ["a", "show bundler address QR code"],
+      ["h", "show this help message"],
+      ["ctrl-c", "quit"],
+    ].forEach(([key, description]) => {
+      terminal.log(press + key + dim(` to ${description}.`));
+    });
+  };
+
   // create a reporter function, to be bound to the Metro configuration.
   // which writes to the Metro terminal and
   // also notifies the `reportEvent` delegate.
@@ -103,16 +117,7 @@ export async function rnxStart(
         reportEventDelegate(event);
       }
       if (interactive && event.type === "dep_graph_loading") {
-        const dim = chalk.dim;
-        const press = dim(" › Press ");
-        [
-          ["r", "reload the app"],
-          ["d", "open developer menu"],
-          ["a", "show bundler address QR code"],
-          ["ctrl-c", "quit"],
-        ].forEach(([key, description]) => {
-          terminal.log(press + key + dim(` to ${description}.`));
-        });
+        printHelp();
       }
     },
   };
@@ -245,6 +250,10 @@ export async function rnxStart(
           case "d":
             terminal.log(chalk.green("Opening developer menu..."));
             messageSocketEndpoint.broadcast("devMenu", undefined);
+            break;
+
+          case "h":
+            printHelp();
             break;
 
           case "r":
