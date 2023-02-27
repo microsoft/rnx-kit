43c7960ed6f65b5c845d83b8b762d1aa15ab4273
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-02-27 13:21:20 +0100

    feat(react-native-host): ability to release the bridge if unused

--- packages/react-native-host/cocoa/RNXHostConfig.h
+++ packages/react-native-host/cocoa/RNXHostConfig.h
@@ -13,6 +13,9 @@ NS_ASSUME_NONNULL_BEGIN
 /// Returns whether the loading view should be visible while loading JavaScript.
 @property (nonatomic, readonly) BOOL isDevLoadingViewEnabled;
 
+/// Returns whether the bridge should be released when the app is backgrounded.
+@property (nonatomic, readonly) BOOL shouldReleaseBridgeWhenBackgrounded;
+
 /// Logs a message.
 - (void)logWithLevel:(RCTLogLevel)level
               source:(RCTLogSource)source
@@ -20,6 +23,15 @@ NS_ASSUME_NONNULL_BEGIN
                 line:(NSNumber *)line
              message:(NSString *)message
     __attribute__((__swift_name__("log(_:source:filename:line:message:)")));
+
+// Called when the bridge has been instantiated.
+- (void)onBridgeInstantiated:(RCTBridge *)bridge;
+
+// Called when the bridge is about to be shut down.
+- (void)onBridgeWillShutDown:(RCTBridge *)bridge;
+
+// Called when the bridge has been shut down.
+- (void)onBridgeDidShutDown;
 
 /// Handles a fatal error.
 - (void)onFatalError:(NSError *)error;
--- packages/react-native-host/cocoa/ReactNativeHost.mm
+++ packages/react-native-host/cocoa/ReactNativeHost.mm
@@ -4,12 +4,18 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTDevLoadingViewSetEnabled.h>
 #import <React/RCTRootView.h>
+#import <React/RCTUIManager.h>
+#import <React/RCTUIManagerUtils.h>
 #import <React/RCTUtils.h>
 
 #import "RNXHostConfig.h"
 
 @implementation ReactNativeHost {
     __weak id<RNXHostConfig> _config;
+    RCTBridge *_bridge;
+    __weak NSDictionary<NSNumber *, UIView *> *_viewRegistry;
+    NSLock *_isShuttingDown;
+    BOOL _isObservingAppDidEnterBackgroundNotification;
 }
 
 - (instancetype)initWithConfig:(__weak id<RNXHostConfig>)config
@@ -36,15 +42,53 @@
         }
 
         _config = config;
-        _bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
+        _isShuttingDown = [[NSLock alloc] init];
+
+        (void)self.bridge;  // Initialize the bridge now
     }
     return self;
 }
 
+- (RCTBridge *)bridge
+{
+    if (![_isShuttingDown tryLock]) {
+        NSAssert(NO, @"Tried to access the bridge while shutting down");
+        return nil;
+    }
+
+    @try {
+        if (_bridge == nil) {
+            _bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
+            [self releaseBridgeWhenBackgrounded];
+            if ([_config respondsToSelector:@selector(onBridgeInstantiated:)]) {
+                [_config onBridgeInstantiated:_bridge];
+            }
+        }
+
+        return _bridge;
+    } @finally {
+        [_isShuttingDown unlock];
+    }
+}
+
 - (void)shutdown
 {
-    [_bridge invalidate];
-    _bridge = nil;
+    if ([_config respondsToSelector:@selector(onBridgeWillShutDown:)]) {
+        [_config onBridgeWillShutDown:_bridge];
+    }
+
+    [_isShuttingDown lock];
+
+    @try {
+        [_bridge invalidate];
+        _bridge = nil;
+
+        if ([_config respondsToSelector:@selector(onBridgeDidShutDown)]) {
+            [_config onBridgeDidShutDown];
+        }
+    } @finally {
+        [_isShuttingDown unlock];
+    }
 }
 
 - (void)usingModule:(Class)moduleClass block:(void (^)(id<RCTBridgeModule> _Nullable))block
@@ -113,6 +157,63 @@
     return [[RCTRootView alloc] initWithBridge:self.bridge
                                     moduleName:moduleName
                              initialProperties:initialProperties];
+}
+
+// MARK: - Private
+
+- (void)onAppDidEnterBackground:(NSNotification *)note
+{
+    if (_viewRegistry.count == 0) {
+        [self shutdown];
+    }
+}
+
+- (void)releaseBridgeWhenBackgrounded
+{
+#if !TARGET_OS_OSX
+    if (![_config respondsToSelector:@selector(shouldReleaseBridgeWhenBackgrounded)] ||
+        ![_config shouldReleaseBridgeWhenBackgrounded]) {
+        return;
+    }
+
+    // This may initialize `RCTAccessibilityManager` and must therefore be run
+    // on the main queue.
+    __weak typeof(self) weakSelf = self;
+    RCTExecuteOnMainQueue(^{
+      typeof(self) strongSelf = weakSelf;
+      if (strongSelf == nil) {
+          return;
+      }
+
+      RCTBridge *bridge = strongSelf->_bridge;
+      RCTUIManager *manager = bridge.uiManager;
+      if (manager == nil) {
+          return;
+      }
+
+      // `addUIBlock` must be called on the UIManager queue.
+      RCTExecuteOnUIManagerQueue(^{
+        [manager addUIBlock:^(RCTUIManager *uiManager,
+                              NSDictionary<NSNumber *, UIView *> *viewRegistry) {
+          typeof(self) strongSelf = weakSelf;
+          if (strongSelf == nil) {
+              return;
+          }
+
+          strongSelf->_viewRegistry = viewRegistry;
+        }];
+      });
+    });
+
+    if (!_isObservingAppDidEnterBackgroundNotification) {
+        _isObservingAppDidEnterBackgroundNotification = YES;
+        NSNotificationCenter *notificationCenter = NSNotificationCenter.defaultCenter;
+        [notificationCenter addObserver:self
+                               selector:@selector(onAppDidEnterBackground:)
+                                   name:UIApplicationDidEnterBackgroundNotification
+                                 object:nil];
+    }
+#endif  // !TARGET_OS_OSX
 }
 
 @end
