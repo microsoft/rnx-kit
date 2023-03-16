a14b66bf538d224fcc05e1825a05fb10a3c87ca5
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-03-16 15:13:53 +0100

    suppress more warnings

--- packages/react-native-host/cocoa/RNXFabricAdapter.mm
+++ packages/react-native-host/cocoa/RNXFabricAdapter.mm
@@ -1,9 +1,13 @@
 #import "RNXFabricAdapter.h"
 
 #ifdef USE_FABRIC
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcomma"
+#pragma clang diagnostic ignored "-Wdocumentation"
 #import <React/RCTSurfacePresenter.h>
 #import <React/RCTSurfacePresenterBridgeAdapter.h>
 #import <react/config/ReactNativeConfig.h>
+#pragma clang diagnostic pop
 #endif  // USE_FABRIC
 
 NSObject *RNXInstallSurfacePresenterBridgeAdapter(RCTBridge *bridge)
