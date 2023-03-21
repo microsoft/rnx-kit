ad4d49c31f4fb5b7783277e02a030a0ae3ad815c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-03-21 11:28:58 +0100

    feat(commitlint-lite): lint commit messages

---
+++ .changeset/lovely-monkeys-live.md
@@ -0,0 +1,5 @@
+---
+"@rnx-kit/commitlint-lite": major
+---
+
+Lint commit message according to [Conventional Commits](https://www.conventionalcommits.org/en/v1.0.0/)
---
+++ incubator/commitlint-lite/test/index.test.ts
@@ -0,0 +1,34 @@
+const { MAX_LINE_LENGTH } = require("../src/constants.ts");
+const { lint } = require("../src/index.ts");
+
+describe("Lint commit message", () => {
+  it("should fail non-conforming messages", () => {
+    expect(lint("")).toEqual(["empty"]);
+    expect(lint("fix")).toEqual(["format"]);
+    expect(lint("foo:")).toEqual(["type", "title"]);
+    expect(lint("Fix:")).toEqual(["type-case", "title"]);
+    expect(lint("fix:")).toEqual(["title"]);
+    expect(lint("fix: ")).toEqual(["title"]);
+    expect(lint("fix():")).toEqual(["scope", "title"]);
+    expect(lint("fix(): ")).toEqual(["scope", "title"]);
+    expect(lint("fix(Scope): ")).toEqual(["scope-case", "title"]);
+    expect(lint("fix:title")).toEqual(["space-after-colon"]);
+    expect(lint("fix: title\nbody")).toEqual(["paragraph"]);
+    expect(lint("fix():title\nbody")).toEqual([
+      "scope",
+      "space-after-colon",
+      "paragraph",
+    ]);
+    expect(lint(`fix: title\n\n${"x".repeat(MAX_LINE_LENGTH + 1)}`)).toEqual([
+      "body-line-length",
+    ]);
+  });
+
+  it("should pass conforming messages", () => {
+    expect(lint("fix: title\n\nbody")).toEqual([]);
+    expect(lint("fix(scope): title\n\nbody")).toEqual([]);
+    expect(lint("fix!: title\n\nbody")).toEqual([]);
+    expect(lint("fix(scope)!: title\n\nbody")).toEqual([]);
+    expect(lint(`fix: title\n\n${"x".repeat(MAX_LINE_LENGTH)}`)).toEqual([]);
+  });
+});
