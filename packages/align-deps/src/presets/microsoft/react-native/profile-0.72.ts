f81d54c4f443acdc299b8179ac2afeea0a4568f1
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-06-05 12:50:52 +0200

    update deps for rc.5

--- packages/align-deps/src/presets/microsoft/react-native/profile-0.72.ts
+++ packages/align-deps/src/presets/microsoft/react-native/profile-0.72.ts
@@ -41,59 +41,59 @@ const profile: Profile = {
 
   "babel-preset-react-native": {
     name: "metro-react-native-babel-preset",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "community/cli": {
     name: "@react-native-community/cli",
-    version: "^11.2.3",
+    version: "^11.3.1",
     capabilities: ["community/cli-android", "community/cli-ios"],
     devOnly: true,
   },
   "community/cli-android": {
     name: "@react-native-community/cli-platform-android",
-    version: "^11.2.3",
+    version: "^11.3.1",
     devOnly: true,
   },
   "community/cli-ios": {
     name: "@react-native-community/cli-platform-ios",
-    version: "^11.2.3",
+    version: "^11.3.1",
     devOnly: true,
   },
   metro: {
     name: "metro",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "metro-config": {
     name: "metro-config",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "metro-core": {
     name: "metro-core",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "metro-react-native-babel-transformer": {
     name: "metro-react-native-babel-transformer",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "metro-resolver": {
     name: "metro-resolver",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
   "metro-runtime": {
     name: "metro-runtime",
-    version: "^0.76.4",
+    version: "^0.76.5",
     devOnly: true,
   },
 
   animation: {
     name: "react-native-reanimated",
-    version: "^3.1.0",
+    version: "^3.2.0",
   },
   "datetime-picker": {
     name: "@react-native-community/datetimepicker",
