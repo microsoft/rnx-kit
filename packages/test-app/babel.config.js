1416bbcdb69965c09a36f2f263368f7405e43393
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-07-10 14:19:57 +0200

    fix(test-app): fix 'duplicate `__self` prop found' error (#2510)

--- packages/test-app/babel.config.js
+++ packages/test-app/babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-  presets: ["@rnx-kit/babel-preset-metro-react-native"],
+  presets: [
+    [
+      "@rnx-kit/babel-preset-metro-react-native",
+      { useTransformReactJSXExperimental: true },
+    ],
+  ],
+  overrides: [
+    {
+      plugins: [
+        [
+          require("@babel/plugin-transform-react-jsx"),
+          { runtime: "automatic" },
+        ],
+        [require("@babel/plugin-transform-react-jsx-source")],
+      ],
+    },
+  ],
 };
