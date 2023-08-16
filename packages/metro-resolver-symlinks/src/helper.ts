621f5b19b68cdc5660f2eb4439fa550c915c13a7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-08-16 18:36:17 +0200

    refactor(metro-resolver-symlinks): reuse `findMetroPath`

--- packages/metro-resolver-symlinks/src/helper.ts
+++ packages/metro-resolver-symlinks/src/helper.ts
@@ -1,7 +1,5 @@
-import {
-  findPackageDependencyDir,
-  readPackage,
-} from "@rnx-kit/tools-node/package";
+import { findPackageDependencyDir } from "@rnx-kit/tools-node/package";
+import { findMetroPath } from "@rnx-kit/tools-react-native/metro";
 
 export function resolveFrom(
   moduleName: string,
@@ -11,31 +9,6 @@ export function resolveFrom(
     startDir,
     resolveSymlinks: true,
   });
-}
-
-export function ensureResolveFrom(
-  moduleName: string,
-  startDir: string
-): string {
-  const p = resolveFrom(moduleName, startDir);
-  if (!p) {
-    throw new Error(`Cannot find module '${moduleName}'`);
-  }
-  return p;
-}
-
-/**
- * Returns the directory in which `metro` and its dependencies were installed.
- * @param fromDir The directory to start searching from
- */
-export function getMetroSearchPath(fromDir = process.cwd()): string {
-  const rnPath = ensureResolveFrom("react-native", fromDir);
-  const rncliPath = ensureResolveFrom("@react-native-community/cli", rnPath);
-
-  const { dependencies = {} } = readPackage(rncliPath);
-  return "@react-native-community/cli-plugin-metro" in dependencies
-    ? ensureResolveFrom("@react-native-community/cli-plugin-metro", rncliPath)
-    : rncliPath;
 }
 
 /**
@@ -49,13 +22,22 @@ export function requireModuleFromMetro<R = unknown>(
   moduleName: string,
   fromDir = process.cwd()
 ): R {
-  try {
-    const startDir = getMetroSearchPath(fromDir);
-    const metroModulePath = ensureResolveFrom(moduleName, startDir);
-    return require(metroModulePath);
-  } catch (_) {
+  const startDir = findMetroPath(fromDir);
+  if (!startDir) {
+    throw new Error(`Cannot find module '${moduleName}'`);
+  }
+
+  const modulePath = resolveFrom(moduleName, startDir);
+  if (!modulePath) {
     throw new Error(
-      `Cannot find module '${moduleName}'. This probably means that '@rnx-kit/metro-resolver-symlinks' is not compatible with the version of 'metro' that you are currently using. Please update to the latest version and try again. If the issue still persists after the update, please file a bug at https://github.com/microsoft/rnx-kit/issues.`
+      `Cannot find module '${moduleName}'. This probably means that ` +
+        "'@rnx-kit/metro-resolver-symlinks' is not compatible with the " +
+        "version of 'metro' that you are currently using. Please update to " +
+        "the latest version and try again. If the issue still persists after " +
+        "the update, please file a bug at " +
+        "https://github.com/microsoft/rnx-kit/issues."
     );
   }
+
+  return require(modulePath);
 }
