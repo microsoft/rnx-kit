84e89a5056dd8d6b0f7c3e7c4276cbb600c43c4c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-08-23 12:19:18 +0200

    share snapshots

--- packages/react-native-lazy-index/test/index.test.ts
+++ packages/react-native-lazy-index/test/index.test.ts
@@ -6,6 +6,33 @@ describe("react-native-lazy-index", () => {
 
   const currentWorkingDir = process.cwd();
 
+  const snapshotMyAwesomeApp = `
+const {
+  AppRegistry
+} = require("react-native");
+const BatchedBridge = require("react-native/Libraries/BatchedBridge/BatchedBridge");
+BatchedBridge.registerLazyCallableModule("YetAnotherFeature", () => {
+  require("@awesome-app/yet-another-feature");
+  return BatchedBridge.getCallableModule("YetAnotherFeature");
+});
+BatchedBridge.registerLazyCallableModule("YetAnotherFeatureLazy", () => {
+  require("@awesome-app/yet-another-feature");
+  return BatchedBridge.getCallableModule("YetAnotherFeatureLazy");
+});
+BatchedBridge.registerLazyCallableModule("AnotherFeature", () => {
+  require("@awesome-app/another-feature");
+  return BatchedBridge.getCallableModule("AnotherFeature");
+});
+AppRegistry.registerComponent("SomeFeature", () => {
+  require("@awesome-app/some-feature");
+  return AppRegistry.getRunnable("SomeFeature").componentProvider();
+});
+AppRegistry.registerComponent("FinalFeature", () => {
+  require("@awesome-app/final-feature");
+  return AppRegistry.getRunnable("FinalFeature").componentProvider();
+});
+`.trim();
+
   /**
    * Tests the specified fixture.
    */
@@ -41,19 +68,45 @@ describe("react-native-lazy-index", () => {
   test("wraps AppRegistry.registerComponent calls", () => {
     const result = transformFixture("AppRegistry");
     expect(result).toBeTruthy();
-    expect(result?.code).toMatchSnapshot();
+    expect(result?.code).toBe(
+      `
+const {
+  AppRegistry
+} = require("react-native");
+AppRegistry.registerComponent("Component-01", () => {
+  require("./component-01");
+  return AppRegistry.getRunnable("Component-01").componentProvider();
+});
+AppRegistry.registerComponent("Component-02", () => {
+  require("./component-02");
+  return AppRegistry.getRunnable("Component-02").componentProvider();
+});
+`.trim()
+    );
   });
 
   test("wraps BatchedBridge.registerCallableModule calls", () => {
     const result = transformFixture("BatchedBridge");
     expect(result).toBeTruthy();
-    expect(result?.code).toMatchSnapshot();
+    expect(result?.code).toBe(
+      `
+const BatchedBridge = require("react-native/Libraries/BatchedBridge/BatchedBridge");
+BatchedBridge.registerLazyCallableModule("Module-01", () => {
+  require("./module-01");
+  return BatchedBridge.getCallableModule("Module-01");
+});
+BatchedBridge.registerLazyCallableModule("Module-02", () => {
+  require("./module-02");
+  return BatchedBridge.getCallableModule("Module-02");
+});
+`.trim()
+    );
   });
 
   test("wraps components declared in `package.json`", () => {
     const result = transformFixture("MyAwesomeApp");
     expect(result).toBeTruthy();
-    expect(result?.code).toMatchSnapshot();
+    expect(result?.code).toBe(snapshotMyAwesomeApp);
   });
 
   test("wraps components declared inline", () => {
@@ -65,6 +118,6 @@ describe("react-native-lazy-index", () => {
       FinalFeature: "@awesome-app/final-feature",
     });
     expect(result).toBeTruthy();
-    expect(result?.code).toMatchSnapshot();
+    expect(result?.code).toBe(snapshotMyAwesomeApp);
   });
 });
