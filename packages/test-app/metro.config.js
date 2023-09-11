e351d059918a134650da0af623a98414acf28029
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-09-11 17:15:27 +0200

    fix bundle config

--- packages/test-app/metro.config.js
+++ packages/test-app/metro.config.js
@@ -1,9 +1,16 @@
-const { exclusionList, makeMetroConfig } = require("@rnx-kit/metro-config");
+const {
+  exclusionList,
+  makeMetroConfig,
+  resolveUniqueModule,
+} = require("@rnx-kit/metro-config");
 const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 const path = require("node:path");
 
 // If USE_AUTH_MOCK=1, exclude the real module to enable the mock.
 const useAuthMock = process.env["USE_AUTH_MOCK"];
+
+const [normalizeColorsPath, normalizeColorsExcludePattern] =
+  resolveUniqueModule("@react-native/normalize-colors");
 
 const blockList = exclusionList([
   // Metro will pick up react-native-macos/-windows mocks if we don't exclude them
@@ -11,11 +18,14 @@ const blockList = exclusionList([
 
   // If USE_AUTH_MOCK=1, exclude the real module to enable the mock.
   ...(useAuthMock ? [/.*packages[/\\]react-native-auth.*/] : []),
+
+  normalizeColorsExcludePattern,
 ]);
 
 module.exports = makeMetroConfig({
   resolver: {
     extraNodeModules: {
+      "@react-native/normalize-colors": normalizeColorsPath,
       internal: path.resolve(__dirname, "src", "internal"),
       ...(useAuthMock
         ? {
