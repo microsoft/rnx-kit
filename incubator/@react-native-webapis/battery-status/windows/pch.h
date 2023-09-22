a103f1c52461e71aa593e91a87b913902c930b0c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-09-22 03:40:25 -0700

    add preliminary Windows implementation

---
+++ incubator/@react-native-webapis/battery-status/windows/pch.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define NOMINMAX
+
+#include <hstring.h>
+#include <restrictederrorinfo.h>
+#include <unknwn.h>
+#include <windows.h>
+#include <CppWinRTIncludes.h>
+#if __has_include(<VersionMacros.h>)
+  #include <VersionMacros.h>
+#endif
+
+#include <winrt/Microsoft.ReactNative.h>
