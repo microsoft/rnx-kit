7971204f17e0df8b9935f5ef307aa63025ef174c
Lorenzo Sciandra <lsciandra@microsoft.com>
2023-09-27 15:11:31 +0100

    do a few updates, tweak code accordingly

--- docsite/docusaurus.config.js
+++ docsite/docusaurus.config.js
@@ -2,12 +2,13 @@
 
 // Ensure content is generated
 require("./generate")();
+const { themes } = require('prism-react-renderer');
 
-const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
+const darkCodeTheme = themes.vsDark;
 
 /** @type {import("prism-react-renderer").PrismTheme} */
 // @ts-expect-error Type definition doesn't match up with actual export
-const lightCodeTheme = require("prism-react-renderer/themes/vsLight");
+const lightCodeTheme = themes.vsLight;
 lightCodeTheme.plain.color = "rgb(28, 30, 33)"; // from CSS --font-color-base-rgb
 lightCodeTheme.plain.backgroundColor = "#ffffff";
 
