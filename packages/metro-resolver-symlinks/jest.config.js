f056c1f4a36600633a4a10f7d6ab884684df6b5b
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-11-06 16:57:42 +0100

    fixup! test(metro-resolver-symlinks): address pnpm issues

--- packages/metro-resolver-symlinks/jest.config.js
+++ packages/metro-resolver-symlinks/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: "@rnx-kit/jest-preset/private",
   moduleNameMapper: {
-    "__fixtures__/.*/metro-resolver$": require.resolve("metro-resolver"),
+    "__fixtures__[/\\\\].*[/\\\\]metro-resolver$":
+      require.resolve("metro-resolver"),
   },
 };
