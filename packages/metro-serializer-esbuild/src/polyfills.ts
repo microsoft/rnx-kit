c73cc2e8bbc8706009c282707df834db2cccf4ab
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-11-20 14:18:03 +0100

    fix(metro-serializer-esbuild): polyfill `Symbol.asyncIterator` as necessary

--- packages/metro-serializer-esbuild/src/polyfills.ts
+++ packages/metro-serializer-esbuild/src/polyfills.ts
@@ -1,10 +1,18 @@
+import { v } from "./version";
+
 function isHermes(target: string | string[]): boolean {
   const isHermes = (t: string) => t.startsWith("hermes");
   return Array.isArray(target) ? target.some(isHermes) : isHermes(target);
 }
 
-export function polyfillAsyncIteratorSymbol(target: string | string[]): string {
-  return isHermes(target)
+export function polyfillAsyncIteratorSymbol(
+  esbuild: { version: string },
+  target: string | string[]
+): string {
+  // No longer needed in 0.19.6:
+  // https://github.com/evanw/esbuild/releases/tag/v0.19.6
+  const isNeeded = v(esbuild.version) < v("0.19.6");
+  return isNeeded && isHermes(target)
     ? `if (!Symbol.asyncIterator) { Symbol.asyncIterator = Symbol.for("Symbol.asyncIterator"); }`
     : "";
 }
--- packages/metro-serializer-esbuild/test/polyfillAsyncIteratorSymbol.test.ts
+++ packages/metro-serializer-esbuild/test/polyfillAsyncIteratorSymbol.test.ts
@@ -1,18 +1,64 @@
 import { polyfillAsyncIteratorSymbol } from "../src/polyfills";
 
 describe("polyfillAsyncIteratorSymbol()", () => {
+  const esbuild = {
+    "0.18.8": { version: "0.18.8" }, // Lowering `async` generator functions introduced
+    "0.19.5": { version: "0.19.5" },
+    "0.19.6": { version: "0.19.6" }, // `Symbol.asyncIterator` introduced
+  };
+
   test("returns empty string for non-Hermes targets", () => {
-    expect(polyfillAsyncIteratorSymbol("es5")).toBeFalsy();
-    expect(polyfillAsyncIteratorSymbol("es6")).toBeFalsy();
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.18.8"], "es5")).toBeFalsy();
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.18.8"], "es6")).toBeFalsy();
+
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.19.5"], "es5")).toBeFalsy();
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.19.5"], "es6")).toBeFalsy();
+
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.19.6"], "es5")).toBeFalsy();
+    expect(polyfillAsyncIteratorSymbol(esbuild["0.19.6"], "es6")).toBeFalsy();
   });
 
   test("returns polyfill for Hermes targets", () => {
-    expect(polyfillAsyncIteratorSymbol("hermes")).toBeTruthy();
-    expect(polyfillAsyncIteratorSymbol("hermes0.7.0")).toBeTruthy();
-    expect(polyfillAsyncIteratorSymbol("hermes0.12.0")).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.18.8"], "hermes")
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.18.8"], "hermes0.7.0")
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.18.8"], "hermes0.12.0")
+    ).toBeTruthy();
+
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.5"], "hermes")
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.5"], "hermes0.7.0")
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.5"], "hermes0.12.0")
+    ).toBeTruthy();
+
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.6"], "hermes")
+    ).toBeFalsy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.6"], "hermes0.7.0")
+    ).toBeFalsy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.6"], "hermes0.12.0")
+    ).toBeFalsy();
   });
 
   test("returns polyfill if Hermes is one of the targets", () => {
-    expect(polyfillAsyncIteratorSymbol(["es6", "hermes0.7.0"])).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.18.8"], ["es6", "hermes0.7.0"])
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.5"], ["es6", "hermes0.7.0"])
+    ).toBeTruthy();
+    expect(
+      polyfillAsyncIteratorSymbol(esbuild["0.19.6"], ["es6", "hermes0.7.0"])
+    ).toBeFalsy();
   });
 });
