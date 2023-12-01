456d0b82a4ce52c65a177ddfaced01bd52661ad8
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2023-12-01 13:18:35 +0100

    feat(react-native-host): bump C++ language standard to C++20
    This will unfortunately break `react-native` versions 0.65 and below,
    but is neccessary to support 0.74 and above.

--- packages/react-native-host/cocoa/RNXTurboModuleAdapter.mm
+++ packages/react-native-host/cocoa/RNXTurboModuleAdapter.mm
@@ -1,6 +1,12 @@
 #import "RNXTurboModuleAdapter.h"
 
-#define FOLLY_NO_CONFIG 1
+#include <folly/Portability.h>
+#if FOLLY_HAS_COROUTINES
+// TODO: `FOLLY_CFG_NO_COROUTINES` was added in 0.73. We can drop this block
+// when we drop support for 0.72:
+// https://github.com/facebook/react-native/commit/17154a661fe06ed25bf599f47bd4193eba011971
+#define FOLLY_HAS_COROUTINES 0
+#endif
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wcomma"
 #import <cxxreact/JSExecutor.h>
