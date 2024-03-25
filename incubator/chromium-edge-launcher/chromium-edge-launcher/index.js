cbb5f4fe629a721c3a6c968857e5f40e5f4b333a
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-03-25 12:19:40 +0100

    chore: remove `chromium-edge-launcher` fork (#3023)
    See https://github.com/facebook/react-native/pull/43524

--- incubator/chromium-edge-launcher/chromium-edge-launcher/index.js
+++
@@ -1,4 +0,0 @@
-// This provides legacy support for a previously documented require pattern
-//    const edgeLauncher = require('chromium-edge-launcher/chromium-edge-launcher');
-
-module.exports = require("../");
--- incubator/chromium-edge-launcher/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { Launcher, killAll, launch } from "./edge-launcher";
-export type {
-  LaunchedEdge,
-  ModuleOverrides,
-  Options,
-  RimrafModule,
-} from "./edge-launcher";
