ff4fe4ce36e8de9691f238113674c18d58d979ba
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-04-02 13:42:19 +0200

    fix(console): inline colors and drop `chalk` (#3043)

--- packages/console/src/index.ts
+++ packages/console/src/index.ts
@@ -1,12 +1,21 @@
-import chalk from "chalk";
+import { WriteStream } from "tty";
 
 type Log = typeof console.log;
 
-export const error: Log = (...args) =>
-  console.error(chalk.red.bold("error"), ...args);
+let errorTag: string;
+let infoTag: string;
+let warnTag: string;
 
-export const info: Log = (...args) =>
-  console.log(chalk.cyan.bold("info"), ...args);
+if (WriteStream.prototype.hasColors() && process.env["NODE_ENV"] !== "test") {
+  errorTag = "\u001B[31m\u001B[1merror\u001B[22m\u001B[39m";
+  infoTag = "\u001B[36m\u001B[1minfo\u001B[22m\u001B[39m";
+  warnTag = "\u001B[33m\u001B[1mwarn\u001B[22m\u001B[39m";
+} else {
+  errorTag = "error";
+  infoTag = "info";
+  warnTag = "warn";
+}
 
-export const warn: Log = (...args) =>
-  console.warn(chalk.yellow.bold("warn"), ...args);
+export const error: Log = (...args) => console.error(errorTag, ...args);
+export const info: Log = (...args) => console.log(infoTag, ...args);
+export const warn: Log = (...args) => console.warn(warnTag, ...args);
