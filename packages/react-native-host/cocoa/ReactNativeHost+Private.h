76f92c11db0ddff747913cc741ac8ae5e0889a12
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-05-07 18:14:40 +0200

    fix(react-native-host): fix `SurfaceRegistryBinding::startSurface` failed (#3130)

--- packages/react-native-host/cocoa/ReactNativeHost+Private.h
+++ packages/react-native-host/cocoa/ReactNativeHost+Private.h
@@ -1,10 +1,16 @@
 #import "ReactNativeHost.h"
 
+@class RCTHost;
 @class RCTSurfacePresenter;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ReactNativeHost (Private)
+
+/// Returns the current ``RCTHost`` instance.
+///
+/// - Note: Returns `nil` if New Architecture is not enabled.
+@property (nonatomic, readonly, nullable) RCTHost *reactHost;
 
 /// Returns the current ``RCTSurfacePresenter`` instance.
 ///
