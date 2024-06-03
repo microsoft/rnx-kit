7c6a036f584bfedab02da5509125c7407b96d1d7
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-06-03 16:32:10 +0200

    feat(align-deps): add Yarn plugin

---
+++ incubator/yarn-plugin-dynamic-extensions/README.md
@@ -0,0 +1,33 @@
+# @rnx-kit/yarn-plugin-dynamic-extensions
+
+[![Build](https://github.com/microsoft/rnx-kit/actions/workflows/build.yml/badge.svg)](https://github.com/microsoft/rnx-kit/actions/workflows/build.yml)
+[![npm version](https://img.shields.io/npm/v/@rnx-kit/yarn-plugin-dynamic-extensions)](https://www.npmjs.com/package/@rnx-kit/yarn-plugin-dynamic-extensions)
+
+🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
+
+### THIS TOOL IS EXPERIMENTAL — USE WITH CAUTION
+
+🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
+
+This is a sample folder to use as base for generating new packages for
+`rnx-kit`.
+
+## Motivation
+
+We want new packages to follow an existing set of patterns and guidelines; via
+this package, we can enforce easily allow new folders to stick to at least a
+common starting point.
+
+## Installation
+
+```sh
+yarn add @rnx-kit/yarn-plugin-dynamic-extensions --dev
+```
+
+or if you're using npm
+
+```sh
+npm add --save-dev @rnx-kit/yarn-plugin-dynamic-extensions
+```
+
+## Usage
---
+++ incubator/yarn-plugin-dynamic-extensions/index.js
@@ -0,0 +1,137 @@
+// @ts-check
+
+/**
+ * @import { Hooks, Manifest, PackageExtensionData, Plugin } from "@yarnpkg/core";
+ * @typedef {{ [key: string]: string | number | boolean | JSONObject | JSONObject[] | null }} JSONObject;
+ * @typedef {Manifest["raw"]} RawManifest;
+ * @typedef {{ cwd: string; manifest: RawManifest; }} Workspace;
+ */
+
+const PLUGIN_NAME = "@rnx-kit/yarn-plugin-dynamic-extensions";
+
+/**
+ * @param {RawManifest} manifest
+ * @returns {JSONObject | undefined}
+ */
+function getKitConfig({ "rnx-kit": rnxconfig }) {
+  if (!rnxconfig || typeof rnxconfig !== "object") {
+    return undefined;
+  }
+
+  return /** @type {JSONObject} */ (rnxconfig);
+}
+
+/**
+ * @param {RawManifest} manifest
+ * @param {string} projectRoot
+ * @param {NodeJS.Require} require
+ * @returns {Promise<((ws: Workspace) => PackageExtensionData | undefined) | void>}
+ */
+async function loadUserExtensions(manifest, projectRoot, require) {
+  const rnxconfig = getKitConfig(manifest);
+  if (!rnxconfig || !("packageExtensions" in rnxconfig)) {
+    return;
+  }
+
+  const { packageExtensions } = rnxconfig;
+  if (!packageExtensions) {
+    return;
+  }
+
+  if (typeof packageExtensions === "string") {
+    const path = require("node:path");
+    const { pathToFileURL } = require("node:url");
+
+    // On Windows, import paths must include the `file:` protocol.
+    const url = pathToFileURL(path.resolve(projectRoot, packageExtensions));
+    const external = await import(url.toString());
+    return external?.default ?? external;
+  }
+
+  if (
+    typeof packageExtensions === "object" &&
+    !Array.isArray(packageExtensions)
+  ) {
+    return ({ manifest }) => {
+      const rnxconfig = getKitConfig(manifest);
+      if (!rnxconfig || !("profile" in rnxconfig)) {
+        return;
+      }
+
+      const { profile } = rnxconfig;
+      if (typeof profile !== "string") {
+        return;
+      }
+
+      const p = packageExtensions[profile];
+      if (!p || typeof p !== "object" || Array.isArray(p)) {
+        return;
+      }
+
+      return /** @type {PackageExtensionData} */ (p);
+    };
+  }
+
+  console.warn(
+    `${PLUGIN_NAME}: invalid configuration: 'packageExtensions' must be an object or a string`
+  );
+}
+
+// This module *must* be CommonJS because `actions/setup-node` (and probably
+// others) does not support ESM. Yarn itself does.
+exports.name = PLUGIN_NAME;
+
+/** @type {(require: NodeJS.Require) => Plugin<Hooks>} */
+exports.factory = (require) => ({
+  hooks: {
+    registerPackageExtensions: async (
+      configuration,
+      registerPackageExtension
+    ) => {
+      const { projectCwd } = configuration;
+      if (!projectCwd) {
+        return;
+      }
+
+      const { Project, structUtils } = require("@yarnpkg/core");
+
+      const { workspace } = await Project.find(configuration, projectCwd);
+      if (!workspace) {
+        return;
+      }
+
+      // On Windows, paths are prefixed with `/`. We need to remove it because
+      // Node doesn't handle them very well by default.
+      /** @type {(p: string) => string} */
+      const normalizePath =
+        process.platform === "win32" ? (s) => s.substring(1) : (s) => s;
+
+      const { manifest, project } = workspace;
+      const getUserExtensions = await loadUserExtensions(
+        manifest.raw,
+        normalizePath(projectCwd),
+        require
+      );
+      if (!getUserExtensions) {
+        return;
+      }
+
+      project.workspacesByCwd.forEach(({ cwd, manifest }) => {
+        const { name, version, raw } = manifest;
+        if (!name || !version) {
+          return;
+        }
+
+        /** @type {Workspace} */
+        const workspace = { cwd: normalizePath(cwd), manifest: raw };
+        const data = getUserExtensions(workspace);
+        if (!data) {
+          return;
+        }
+
+        const descriptor = structUtils.makeDescriptor(name, version);
+        registerPackageExtension(descriptor, data);
+      });
+    },
+  },
+});
