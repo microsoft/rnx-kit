65efdab27affa727a69336a05c2ff4ec92982be9
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-06-19 11:30:24 +0200

    fix(react-native-host): override feature flags for bridgeless mode

--- packages/react-native-host/cocoa/RNXBridgelessHeaders.h
+++ packages/react-native-host/cocoa/RNXBridgelessHeaders.h
@@ -11,6 +11,8 @@
 #endif  // USE_HERMES
 
 #import <react/config/ReactNativeConfig.h>
+#import <react/featureflags/ReactNativeFeatureFlags.h>
+#import <react/featureflags/ReactNativeFeatureFlagsDefaults.h>
 
 #if __has_include(<react/runtime/JSEngineInstance.h>)
 using SharedJSRuntimeFactory = std::shared_ptr<facebook::react::JSEngineInstance>;
@@ -41,6 +43,26 @@ using SharedJSRuntimeFactory = std::shared_ptr<facebook::react::JSRuntimeFactory
 - (RCTModuleRegistry *)getModuleRegistry;      // Deprecated in 0.74, and removed in 0.75
 - (RCTSurfacePresenter *)getSurfacePresenter;  // Deprecated in 0.74, and removed in 0.75
 @end
+
+// https://github.com/facebook/react-native/blob/0.74-stable/packages/react-native/Libraries/AppDelegate/RCTAppDelegate.mm#L272-L286
+class RNXBridgelessFeatureFlags : public facebook::react::ReactNativeFeatureFlagsDefaults
+{
+public:
+    bool useModernRuntimeScheduler() override
+    {
+        return true;
+    }
+
+    bool enableMicrotasks() override
+    {
+        return true;
+    }
+
+    bool batchRenderingUpdatesInEventLoop() override
+    {
+        return true;
+    }
+};
 
 #elif USE_FABRIC
 
