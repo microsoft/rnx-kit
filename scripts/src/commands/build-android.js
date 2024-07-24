a7cf12194cb3871633a6c85d4e5a3f3fd0dfb898
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-07-24 20:16:27 +0200

    chore: refactor `build-android` command to use common spawn options (#3241)

--- scripts/src/commands/build-android.js
+++ scripts/src/commands/build-android.js
@@ -1,23 +1,12 @@
 // @ts-check
 
-import { spawn } from "node:child_process";
 import * as os from "node:os";
 import * as path from "node:path";
+import { spawn } from "../process.js";
 
 /** @type {import("../process.js").Command} */
-export function buildAndroid(_args, rawArgs) {
+export async function buildAndroid(_args, rawArgs = []) {
   const wrapper = os.platform() === "win32" ? "gradlew.bat" : "gradlew";
   const gradlew = path.resolve("android", wrapper);
-  return new Promise((resolve, reject) => {
-    spawn(gradlew, rawArgs ?? [], {
-      cwd: path.dirname(gradlew),
-      stdio: "inherit",
-    }).on("close", (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject();
-      }
-    });
-  });
+  await spawn(gradlew, rawArgs, { cwd: path.dirname(gradlew) });
 }
