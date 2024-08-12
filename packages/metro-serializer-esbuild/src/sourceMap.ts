00b0f44183c1a86cf6d3240d7c89eb648c3b2229
Andrew Coates <30809111+acoates-ms@users.noreply.github.com>
2024-08-12 11:41:08 -0700

    Handle updated exports for newer metro versions

--- packages/metro-serializer-esbuild/src/sourceMap.ts
+++ packages/metro-serializer-esbuild/src/sourceMap.ts
@@ -26,9 +26,13 @@ export function absolutizeSourceMap(outputPath: string, text: string): string {
 
 export function getInlineSourceMappingURL(modules: readonly Module[]): string {
   const metroPath = findMetroPath() || "metro";
-  const sourceMapString = require(
+  let sourceMapString = require(
     `${metroPath}/src/DeltaBundler/Serializers/sourceMapString`
   );
+  // Newer versions of metro export an object: https://github.com/facebook/metro/commit/34148e61200a508923315fbe387b26d1da27bf4b#diff-1b836d1729e527a725305eef0cec22e44605af2700fa413f4c2489ea1a03aebc
+  if (sourceMapString.sourceMapString) {
+    sourceMapString = sourceMapString.sourceMapString;
+  }
   const sourceMap = sourceMapString(modules, sourceMappingOptions);
   const base64 = Buffer.from(sourceMap).toString("base64");
   return `data:application/json;charset=utf-8;base64,${base64}`;
