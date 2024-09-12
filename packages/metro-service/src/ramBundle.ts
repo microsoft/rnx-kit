1f1729ac0d759c3d9b0756182364ffc255a46d53
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-09-12 11:21:37 +0200

    fix(metro-service): add RAM bundle deprecation warning

--- packages/metro-service/src/ramBundle.ts
+++ packages/metro-service/src/ramBundle.ts
@@ -1,3 +1,4 @@
+import { warn } from "@rnx-kit/console";
 import type { ConfigT } from "metro-config";
 import * as path from "path";
 import type { BundleArgs } from "./bundle";
@@ -5,6 +6,10 @@ import { bundle } from "./bundle";
 import { requireMetroPath } from "./metro";
 
 export function ramBundle(args: BundleArgs, config: ConfigT): Promise<void> {
+  warn(
+    "RAM bundle is deprecated and was removed in 0.75; for more details, see https://github.com/facebook/react-native/pull/43292"
+  );
+
   const ramBundlePath = path.join(
     requireMetroPath(config.projectRoot),
     "src",
