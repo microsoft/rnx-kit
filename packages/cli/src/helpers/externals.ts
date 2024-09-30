193a34bb9c5c67c4b1d11b32201831f71b86f615
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-09-30 12:00:54 +0200

    feat(cli): support multiple debug targets

--- packages/cli/src/helpers/externals.ts
+++ packages/cli/src/helpers/externals.ts
@@ -1,5 +1,7 @@
 import { resolveDependencyChain } from "@rnx-kit/tools-node/package";
 import { resolveCommunityCLI } from "@rnx-kit/tools-react-native/context";
+import * as fs from "node:fs";
+import * as path from "node:path";
 import type { CliServerApi, CoreDevMiddleware } from "../serve/types";
 
 type ExternalModule =
@@ -7,13 +9,21 @@ type ExternalModule =
   | "@react-native/dev-middleware";
 
 function friendlyRequire<T>(modules: string[], startDir: string): T {
+  const target = modules.pop();
+  if (!target) {
+    throw new Error("At least one target module is required");
+  }
+
+  const resolvedStartDir = fs.lstatSync(startDir).isSymbolicLink()
+    ? path.resolve(path.dirname(startDir), fs.readlinkSync(startDir))
+    : startDir;
   try {
-    const modulePath = resolveDependencyChain(modules, startDir);
-    return require(modulePath) as T;
+    const finalPackageDir = resolveDependencyChain(modules, resolvedStartDir);
+    const targetModule = require.resolve(target, { paths: [finalPackageDir] });
+    return require(targetModule) as T;
   } catch (_) {
-    const module = modules[modules.length - 1];
     throw new Error(
-      `Cannot find module '${module}'. This probably means that ` +
+      `Cannot find module '${target}'. This probably means that ` +
         "'@rnx-kit/cli' is not compatible with the version of 'react-native' " +
         "that you are currently using. Please update to the latest version " +
         "and try again. If the issue still persists after the update, please " +
