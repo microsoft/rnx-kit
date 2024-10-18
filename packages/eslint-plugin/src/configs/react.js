ca0052f84620fc2fe12eb384f24244a3b4e8d02f
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-10-18 12:35:25 +0200

    fix(eslint-plugin): inline `react-hooks` rules to get rid of `@eslint/eslintrc` (#3397)

--- packages/eslint-plugin/src/configs/react.js
+++ packages/eslint-plugin/src/configs/react.js
@@ -1,22 +1,20 @@
 // @ts-check
 "use strict";
 
-const { FlatCompat } = require("@eslint/eslintrc");
 const react = require("eslint-plugin-react");
-const eslint = require("./eslint");
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: eslint.configs.recommended,
-});
+// @ts-expect-error Could not find a declaration file for module
+const reactHooks = require("eslint-plugin-react-hooks");
 
 module.exports = [
-  ...compat.extends("plugin:react-hooks/recommended"),
   react.configs.flat.recommended,
   {
     name: "rnx-kit/react",
+    plugins: {
+      "react-hooks": reactHooks,
+    },
     rules: {
       "react/prop-types": "off",
+      ...reactHooks.configs.recommended.rules,
     },
     settings: {
       react: {
