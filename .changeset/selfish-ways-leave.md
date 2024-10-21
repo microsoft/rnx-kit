419f2ae6295a5712d448a3105a321dbe7e983c60
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-10-21 12:58:21 +0200

    fix(build): make package public (#3403)

---
+++ .changeset/selfish-ways-leave.md
@@ -0,0 +1,5 @@
+---
+"@rnx-kit/build": patch
+---
+
+Make `@rnx-kit/build` public
--- incubator/build/src/index.ts
+++ incubator/build/src/index.ts
@@ -1,5 +1,15 @@
+export { startBuild } from "./build.js";
+export {
+  DEPLOYMENT,
+  DEVICE_TYPES,
+  PLATFORMS,
+  USER_CONFIG_FILE,
+} from "./constants.js";
+export { getDistribution } from "./distribution.js";
 export { getRemoteUrl, getRepositoryRoot, stage } from "./git.js";
+export { detectPackageManager } from "./packageManager.js";
 export { renderQRCode } from "./qrcode.js";
+export { getRemoteInfo } from "./remotes.js";
 export type {
   BuildParams,
   Context,
