5e8f7b0460188c7a68def7f985f6bc9ef08e395b
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-11-25 15:08:18 +0100

    fixup! fix(web-storage): migrate away from deprecated `TurboReactPackage`

--- .changeset/short-dolphins-tap.md
+++ .changeset/short-dolphins-tap.md
@@ -1,5 +1,7 @@
 ---
+"@react-native-webapis/battery-status": minor
 "@react-native-webapis/web-storage": minor
+"@rnx-kit/react-native-test-app-msal": major
 ---
 
 Migrate away from deprecated `TurboReactPackage`. This is a requirement for
--- incubator/@react-native-webapis/battery-status/android/src/main/java/org/reactnativewebapis/batterystatus/BatteryStatusPackage.kt
+++ incubator/@react-native-webapis/battery-status/android/src/main/java/org/reactnativewebapis/batterystatus/BatteryStatusPackage.kt
@@ -1,30 +1,28 @@
 package org.reactnativewebapis.batterystatus
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class BatteryStatusPackage : TurboReactPackage() {
-    override fun getModule(name: String?, reactContext: ReactApplicationContext?): NativeModule {
-        return when (name) {
+class BatteryStatusPackage : BaseReactPackage() {
+    override fun getModule(name: String?, reactContext: ReactApplicationContext?): NativeModule =
+        when (name) {
             BatteryStatusModule.NAME -> BatteryStatusModule(reactContext)
             else -> throw IllegalArgumentException("No module named '$name'")
         }
-    }
 
-    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider =
-        ReactModuleInfoProvider {
-            val info = ReactModuleInfo(
-                BatteryStatusModule.NAME,
-                BatteryStatusModule::class.java.name,
-                false,
-                false,
-                false,
-                false,
-                false
-            )
-            mapOf(info.name() to info).toMutableMap()
-        }
+    override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider {
+        val info = ReactModuleInfo(
+            BatteryStatusModule.NAME,
+            BatteryStatusModule::class.java.name,
+            false,
+            false,
+            false,
+            false,
+            false
+        )
+        mapOf(info.name() to info).toMutableMap()
+    }
 }
