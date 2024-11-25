a71f148ca78d939775d526a6f7eeaba6cbebee2f
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-11-25 12:05:45 +0100

    fix(web-storage): migrate away from deprecated `TurboReactPackage`

---
+++ .changeset/short-dolphins-tap.md
@@ -0,0 +1,6 @@
+---
+"@react-native-webapis/web-storage": minor
+---
+
+Migrate away from deprecated `TurboReactPackage`. This is a requirement for
+supporting 0.77 and above.
--- incubator/@react-native-webapis/web-storage/android/src/main/java/org/reactnativewebapis/webstorage/WebStoragePackage.kt
+++ incubator/@react-native-webapis/web-storage/android/src/main/java/org/reactnativewebapis/webstorage/WebStoragePackage.kt
@@ -1,18 +1,17 @@
 package org.reactnativewebapis.webstorage
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-class WebStoragePackage : TurboReactPackage() {
-    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule {
-        return when (name) {
+class WebStoragePackage : BaseReactPackage() {
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule =
+        when (name) {
             WebStorageModule.NAME -> WebStorageModule(reactContext)
             else -> throw IllegalArgumentException("No module named '$name'")
         }
-    }
 
     override fun getReactModuleInfoProvider(): ReactModuleInfoProvider = ReactModuleInfoProvider {
         val info = ReactModuleInfo(
