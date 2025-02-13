1ecab342aeb6d8b07eb386b947d82d4da8e49366
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2025-02-13 11:42:17 -0800

    fix(eslint-plugin): Fix (dev)dependency cycle in the repo between eslint-config and eslint-plugin (#3518)
    * remove dependency cycle between eslint-config and eslint-plugin
    
    * docs(changeset): Tooling only change to use locally defined eslint.config.js to break dependency cycle
    
    * Update .changeset/forty-doors-itch.md
    
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Tommy Nguyen <4123478+tido64@users.noreply.github.com>

--- packages/eslint-plugin/eslint.config.js
+++ packages/eslint-plugin/eslint.config.js
@@ -1,1 +1,23 @@
-module.exports = require("@rnx-kit/eslint-config");
+const sdl = require("@microsoft/eslint-plugin-sdl");
+const rnx = require("./src");
+
+/**
+ * Note that we don't directly use `sdl.configs.required` because:
+ *
+ *   1. It includes rules for Angular and Electron
+ *   2. Its `react` preset conflicts with our direct use of `eslint-plugin-react`
+ *
+ * https://github.com/microsoft/eslint-plugin-sdl/blob/957996315c80fdadcd1a9f7bb76fc4663d33ef1e/lib/index.js#L47-L54
+ */
+module.exports = [
+  ...sdl.configs.common,
+  ...sdl.configs.node,
+  ...rnx.configs.strict,
+  ...rnx.configs.stylistic,
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      ...sdl.configs.react[0].rules,
+    },
+  },
+];
