66961bede2a728165114a7c22f3ea6eedd310b39
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-02-26 15:58:46 +0100

    fix: support Bun's new lockfile (#3531)

--- packages/tools-workspaces/src/common.ts
+++ packages/tools-workspaces/src/common.ts
@@ -12,6 +12,7 @@ type PackageManager = {
   getPackageFilters: (sentinel: string) => string[] | undefined;
 };
 
+export const BUN_LOCK = "bun.lock";
 export const BUN_LOCKB = "bun.lockb";
 export const LERNA_JSON = "lerna.json";
 export const PACKAGE_LOCK_JSON = "package-lock.json";
@@ -25,6 +26,7 @@ export const WORKSPACE_ROOT_SENTINELS = [
   YARN_LOCK,
   PACKAGE_LOCK_JSON,
   PNPM_WORKSPACE_YAML,
+  BUN_LOCK,
   BUN_LOCKB,
 ];
 
@@ -79,6 +81,7 @@ export const findSentinelSync = makeFindSentinel(findUp.sync);
 
 export function getImplementation(sentinel: string): Promise<PackageManager> {
   switch (path.basename(sentinel)) {
+    case BUN_LOCK: // fallthrough — logic defining workspaces config is the same as for npm and yarn
     case BUN_LOCKB: // fallthrough — logic defining workspaces config is the same as for npm and yarn
     case PACKAGE_LOCK_JSON: // fallthrough — logic defining workspaces config is the same for npm and yarn
     case YARN_LOCK:
@@ -101,6 +104,7 @@ export function getImplementation(sentinel: string): Promise<PackageManager> {
 
 export function getImplementationSync(sentinel: string): PackageManager {
   switch (path.basename(sentinel)) {
+    case BUN_LOCK: // fallthrough — logic defining workspaces config is the same as for npm and yarn
     case BUN_LOCKB: // fallthrough — logic defining workspaces config is the same as for npm and yarn
     case PACKAGE_LOCK_JSON: // fallthrough — logic defining workspaces config is the same for npm and yarn
     case YARN_LOCK:
