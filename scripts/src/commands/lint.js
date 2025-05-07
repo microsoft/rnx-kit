5ea1d4e36cbe0acb33bd155cef3c87aca274c918
Jason Morse <jasonmo@microsoft.com>
2025-05-07 11:28:43 -0700

    inline the lint command to match the other script commands

--- scripts/src/commands/lint.js
+++ scripts/src/commands/lint.js
@@ -25,7 +25,8 @@ export class LintCommand extends Command {
 
   async execute() {
     const args = this.args.length > 0 ? this.args : ["--no-warn-ignored"];
-    await lint(undefined, args);
+    const files = listFiles("*.cjs", "*.js", "*.jsx", "*.mjs", "*.ts", "*.tsx");
+    await runScript("eslint", "--no-warn-ignored", ...files, ...args);
   }
 }
 
@@ -37,10 +38,4 @@ function listFiles(...patterns) {
   const args = ["ls-files", ...patterns];
   const { stdout } = spawnSync("git", args, { encoding: "utf-8" });
   return stdout.trim().split("\n");
-}
-
-/** @type {import("../process.js").Command} */
-export async function lint(_args, rawArgs = ["--no-warn-ignored"]) {
-  const files = listFiles("*.cjs", "*.js", "*.jsx", "*.mjs", "*.ts", "*.tsx");
-  await runScript("eslint", "--no-warn-ignored", ...files, ...rawArgs);
 }
