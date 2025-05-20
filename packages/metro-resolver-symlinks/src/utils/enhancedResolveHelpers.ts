f145c8834c1fa3da0b1893881217cabddf9bd85c
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-05-20 16:47:56 +0000

    fix(metro-resolver-symlinks): add support for 0.82+ (#3627)

--- packages/metro-resolver-symlinks/src/utils/enhancedResolveHelpers.ts
+++ packages/metro-resolver-symlinks/src/utils/enhancedResolveHelpers.ts
@@ -1,6 +1,7 @@
 import { expandPlatformExtensions } from "@rnx-kit/tools-react-native/platform";
 import * as path from "node:path";
 import type { ResolutionContextCompat } from "../types";
+import { isPackageExportsEnabled } from "./metro";
 
 export function getFromDir({
   originModulePath,
@@ -9,16 +10,15 @@ export function getFromDir({
 }
 
 export function makeEnhancedResolveOptions(
-  {
-    extraNodeModules,
-    mainFields,
-    sourceExts,
-    unstable_conditionNames,
-    unstable_enablePackageExports,
-  }: ResolutionContextCompat,
+  context: ResolutionContextCompat,
   platform = "common"
 ) {
+  const { extraNodeModules, mainFields, sourceExts, unstable_conditionNames } =
+    context;
+
   const extensions = sourceExts.map((ext) => `.${ext}`);
+  const enablePackageExports = isPackageExportsEnabled(context);
+
   return {
     // Map Metro's `context.extraNodeModules` to Webpack's `resolve.alias`.
     // Metro's implementation is a subset of Webpack's. See:
@@ -30,13 +30,13 @@ export function makeEnhancedResolveOptions(
     // Add `require` to handle packages that are missing `default`
     // conditional. See
     // https://github.com/webpack/enhanced-resolve/issues/313
-    conditionNames: unstable_enablePackageExports
+    conditionNames: enablePackageExports
       ? unstable_conditionNames.slice()
       : ["require", "node"],
 
     // Unless `unstable_enablePackageExports` is enabled, disable exports
     // map as it currently takes precedence over the `react-native` field.
-    ...(unstable_enablePackageExports ? undefined : { exportsFields: [] }),
+    ...(enablePackageExports ? undefined : { exportsFields: [] }),
 
     extensions:
       platform === "common"
