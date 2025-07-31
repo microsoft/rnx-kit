633af43ad83b7a441323c0a2098f44e2065e990a
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-07-31 16:56:51 +0200

    chore: use `yarn constraints` to lint package metadata (#3718)
    * chore: use `yarn constraints` to lint package metadata
    
    * docs(changeset):

---
+++ yarn.config.cjs
@@ -0,0 +1,33 @@
+// @ts-check
+
+/** @type {import("@yarnpkg/types")} */
+const { defineConfig } = require("@yarnpkg/types");
+
+const IGNORED_LOCATIONS = [".", "packages/test-app", "scripts"];
+
+module.exports = defineConfig({
+  constraints: async ({ Yarn }) => {
+    const root = Yarn.workspace({ cwd: "." });
+    if (!root) {
+      throw new Error("Root workspace not found");
+    }
+
+    const { author, repository: origin } = root.manifest;
+
+    for (const workspace of Yarn.workspaces()) {
+      if (IGNORED_LOCATIONS.includes(workspace.cwd)) {
+        continue;
+      }
+
+      workspace.set("author.name", author.name);
+      workspace.set("author.email", author.email);
+
+      const homepage = `${origin.url}/tree/main/${workspace.cwd}#readme`;
+      workspace.set("homepage", homepage);
+
+      workspace.set("repository.type", origin.type);
+      workspace.set("repository.url", origin.url);
+      workspace.set("repository.directory", workspace.cwd);
+    }
+  },
+});
