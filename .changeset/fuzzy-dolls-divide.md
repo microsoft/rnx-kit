23228ab90133e6736e5c90e83518a1c713ce2330
Fabian Arce <106703105+Farcemolina@users.noreply.github.com>
2025-09-10 11:14:47 -0600

    feat(eslint-plugin): port no-foreach-with-captured-variables rule (#3764)

---
+++ .changeset/fuzzy-dolls-divide.md
@@ -0,0 +1,5 @@
+---
+"@rnx-kit/eslint-plugin": minor
+---
+
+Port no-foreach-with-captured-variables from outlook
---
+++ packages/eslint-plugin/test/no-foreach-with-captured-variables.ts
@@ -0,0 +1,56 @@
+import rule from "../src/rules/no-foreach-with-captured-variables";
+import { makeRuleTester } from "./RuleTester";
+
+describe("disallows `forEach` with variables outside of function scope", () => {
+  const ruleTester = makeRuleTester();
+
+  ruleTester.run("no-foreach-with-captured-variables", rule, {
+    valid: [
+      // Variable inside for each loop
+      {
+        code: `
+            let arr: string[] = [];
+            arr.forEach(a => {
+                let variableDefinedInsideCallback = 1;
+                variableDefinedInsideCallback++;
+            });
+        `,
+      },
+      // Variable inside for each loop and one outside but not call inside of the loop
+      {
+        code: `
+                let variableDefinedOutsideLoop = 1;
+                let arr: string[] = [];
+                arr.forEach(a => {
+                    let variableDefinedInsideCallback = 1;
+                    variableDefinedInsideCallback++;
+                });
+                variableDefinedOutsideLoop++;
+            `,
+      },
+      // Variable outside for of loop
+      {
+        code: `
+            let variableDefinedOutsideLoop = 1;
+            let arr: string[] = [];
+            for (const a of arr) {
+                variableDefinedOutsideLoop++;
+            }
+            `,
+      },
+    ],
+    invalid: [
+      // Variable is outside for each loop
+      {
+        code: `
+            let variableDefinedOutsideLoop = 1;
+            let arr: string[] = [];
+            arr.forEach(a => {
+                variableDefinedOutsideLoop++;
+            });
+            `,
+        errors: 1,
+      },
+    ],
+  });
+});
