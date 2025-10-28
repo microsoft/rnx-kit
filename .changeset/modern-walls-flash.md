1c617955ed6e1b72d12f9610dd46f63238a45429
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2025-10-28 15:37:47 +0000

    fix(eslint-plugin/no-export-all): handle empty exports (#3827)

---
+++ .changeset/modern-walls-flash.md
@@ -0,0 +1,6 @@
+---
+"@rnx-kit/eslint-plugin": patch
+---
+
+Provide a proper error message when a module tries to re-export from another
+module containing no named exports
--- packages/eslint-plugin/src/rules/no-export-all.js
+++ packages/eslint-plugin/src/rules/no-export-all.js
@@ -3,10 +3,9 @@
 
 /**
  * @typedef {import("@typescript-eslint/types").TSESTree.Node} Node
- * @typedef {import("eslint").Linter.Config} Config
  * @typedef {import("eslint").Rule.RuleContext} ESLintRuleContext
  * @typedef {import("eslint").Rule.ReportFixer} ESLintReportFixer
- * @typedef {{ exports: string[], types: string[] }} NamedExports
+ * @typedef {{ exports: string[]; types: string[]; }} NamedExports
  *
  * @typedef {{
  *   id: ESLintRuleContext["id"];
@@ -16,8 +15,8 @@
  *     maxDepth: number;
  *   };
  *   filename: string;
- *   languageOptions: Config["languageOptions"];
- *   parserOptions: ESLintRuleContext["parserOptions"];
+ *   languageOptions: ESLintRuleContext["languageOptions"];
+ *   parserOptions: ESLintRuleContext["languageOptions"]["parserOptions"];
  *   parserPath: ESLintRuleContext["parserPath"];
  *   sourceCode: ESLintRuleContext["sourceCode"];
  * }} RuleContext
@@ -41,6 +40,9 @@ const DEFAULT_CONFIG = {
   eslintVisitorKeys: true,
   eslintScopeManager: true,
 };
+
+const E_EXPORT_ALL = "export-all";
+const E_NO_NAMED_EXPORTS = "no-named-exports";
 
 const NODE_ENV = process.env.NODE_ENV;
 
@@ -161,11 +163,12 @@ function toRuleContext(context) {
       debug: false,
       expand: "all",
       maxDepth: 5,
-      ...(context.options && context.options[0]),
+      ...context.options?.[0],
     },
     filename: context.filename || context.getFilename(),
     languageOptions: context.languageOptions,
-    parserOptions: context.parserOptions,
+    parserOptions:
+      context.languageOptions.parserOptions ?? context.parserOptions,
     parserPath: context.parserPath,
     sourceCode: context.sourceCode,
   };
@@ -202,8 +205,7 @@ function parse(context, moduleId) {
     const parentDir = path.dirname(filename);
     const modulePath = resolveFrom(parentDir, moduleId);
     const code = fs.readFileSync(modulePath, { encoding: "utf-8" });
-    const parserOptions =
-      languageOptions?.parserOptions ?? context.parserOptions;
+    const parserOptions = context.parserOptions ?? {};
     const { project } = parserOptions;
     return {
       ast: parseForESLint(code, {
@@ -225,7 +227,9 @@ function parse(context, moduleId) {
     };
   } catch (e) {
     if (options.debug) {
-      console.error(e);
+      console.error(
+        `[no-export-all] Failed to parse module '${moduleId}': ${e}`
+      );
     }
   }
 
@@ -245,149 +249,139 @@ function extractExports(context, moduleId, depth) {
   }
 
   const parseResult = parse(context, moduleId);
-  if (!parseResult || !parseResult.ast) {
+  if (!parseResult?.ast) {
     return null;
   }
 
   const { ast, filename } = parseResult;
-  try {
-    /** @type {Set<string>} */
-    const exports = new Set();
-    /** @type {Set<string>} */
-    const types = new Set();
 
-    const traverser = makeTraverser();
-    traverser.traverse(ast, {
-      /** @type {(node: Node, parent: Node) => void} */
-      enter: (node, parent) => {
-        switch (node.type) {
-          case "ExportNamedDeclaration": {
-            if (parent.type === "TSModuleBlock") {
-              // The module or namespace is already exported.
-              return;
-            }
+  /** @type {Set<string>} */
+  const exports = new Set();
+  /** @type {Set<string>} */
+  const types = new Set();
 
-            const declaration = node.declaration;
-            if (declaration) {
-              switch (declaration.type) {
-                case "ClassDeclaration":
-                // fallthrough
-                case "FunctionDeclaration":
-                // fallthrough
-                case "TSDeclareFunction":
-                // fallthrough
-                case "TSImportEqualsDeclaration": {
-                  const name = declaration.id && declaration.id.name;
-                  if (name) {
-                    exports.add(name);
-                  }
-                  break;
-                }
-
-                case "TSEnumDeclaration": {
-                  const name = declaration.id && declaration.id.name;
-                  if (name) {
-                    const ex = declaration.const ? types : exports;
-                    ex.add(name);
-                  }
-                  break;
-                }
-
-                // export namespace N { ... }
-                case "TSModuleDeclaration": {
-                  switch (declaration.id.type) {
-                    case "Identifier":
-                      exports.add(declaration.id.name);
-                      break;
-                    case "Literal": {
-                      const name = declaration.id.value;
-                      if (typeof name === "string") {
-                        exports.add(name);
-                      }
-                      break;
-                    }
-                  }
-                  break;
-                }
-
-                case "TSInterfaceDeclaration":
-                // fallthrough
-                case "TSTypeAliasDeclaration": {
-                  const name = declaration.id && declaration.id.name;
-                  if (name) {
-                    types.add(name);
-                  }
-                  break;
-                }
-
-                case "VariableDeclaration":
-                  for (const decl of declaration.declarations) {
-                    if (decl.id.type === "Identifier") {
-                      exports.add(decl.id.name);
-                    }
-                  }
-                  break;
-              }
-            } else {
-              const set = node.exportKind === "type" ? types : exports;
-              for (const spec of node.specifiers) {
-                const exported = spec.exported;
-                const name =
-                  exported.type === "Identifier"
-                    ? exported.name
-                    : exported.value;
-                if (name !== "default") {
-                  set.add(name);
-                }
-              }
-            }
-            break;
+  makeTraverser().traverse(ast, {
+    /** @type {(node: Node, parent: Node) => void} */
+    enter: (node, parent) => {
+      switch (node.type) {
+        case "ExportNamedDeclaration": {
+          if (parent.type === "TSModuleBlock") {
+            // The module or namespace is already exported.
+            return;
           }
 
-          case "ExportAllDeclaration": {
-            const source = node.source && node.source.value;
-            if (source) {
-              const namedExports = extractExports(
-                { ...context, filename },
-                source,
-                depth - 1
-              );
-              if (namedExports) {
-                for (const name of namedExports.exports) {
+          const declaration = node.declaration;
+          if (declaration) {
+            switch (declaration.type) {
+              case "ClassDeclaration":
+              // fallthrough
+              case "FunctionDeclaration":
+              // fallthrough
+              case "TSDeclareFunction":
+              // fallthrough
+              case "TSImportEqualsDeclaration": {
+                const name = declaration.id && declaration.id.name;
+                if (name) {
                   exports.add(name);
                 }
-                for (const name of namedExports.types) {
+                break;
+              }
+
+              case "TSEnumDeclaration": {
+                const name = declaration.id && declaration.id.name;
+                if (name) {
+                  const ex = declaration.const ? types : exports;
+                  ex.add(name);
+                }
+                break;
+              }
+
+              // export namespace N { ... }
+              case "TSModuleDeclaration": {
+                switch (declaration.id.type) {
+                  case "Identifier":
+                    exports.add(declaration.id.name);
+                    break;
+                  case "Literal": {
+                    const name = declaration.id.value;
+                    if (typeof name === "string") {
+                      exports.add(name);
+                    }
+                    break;
+                  }
+                }
+                break;
+              }
+
+              case "TSInterfaceDeclaration":
+              // fallthrough
+              case "TSTypeAliasDeclaration": {
+                const name = declaration.id && declaration.id.name;
+                if (name) {
                   types.add(name);
                 }
+                break;
+              }
+
+              case "VariableDeclaration":
+                for (const decl of declaration.declarations) {
+                  if (decl.id.type === "Identifier") {
+                    exports.add(decl.id.name);
+                  }
+                }
+                break;
+            }
+          } else {
+            const set = node.exportKind === "type" ? types : exports;
+            for (const spec of node.specifiers) {
+              const exported = spec.exported;
+              const name =
+                exported.type === "Identifier" ? exported.name : exported.value;
+              if (name !== "default") {
+                set.add(name);
               }
             }
-            break;
           }
-
-          // Prior to v6, a `TSImportEqualsDeclaration` will have an `isExport`
-          // property instead of being wrapped in an `ExportNamedDeclaration`.
-          // See https://github.com/typescript-eslint/typescript-eslint/issues/4130
-          case "TSImportEqualsDeclaration":
-            if ("isExport" in node && node.isExport) {
-              // export import foo = require('./foo');
-              // export import Bar = Foo.Bar;
-              exports.add(node.id.name);
-            }
-            break;
+          break;
         }
-      },
-    });
-    return {
-      exports: [...exports],
-      types: [...types],
-    };
-  } catch (e) {
-    if (context.options.debug) {
-      console.error(e);
-    }
-  }
 
-  return null;
+        case "ExportAllDeclaration": {
+          const source = node.source && node.source.value;
+          if (source) {
+            const namedExports = extractExports(
+              { ...context, filename },
+              source,
+              depth - 1
+            );
+            if (namedExports) {
+              for (const name of namedExports.exports) {
+                exports.add(name);
+              }
+              for (const name of namedExports.types) {
+                types.add(name);
+              }
+            }
+          }
+          break;
+        }
+
+        // Prior to v6, a `TSImportEqualsDeclaration` will have an `isExport`
+        // property instead of being wrapped in an `ExportNamedDeclaration`.
+        // See https://github.com/typescript-eslint/typescript-eslint/issues/4130
+        case "TSImportEqualsDeclaration":
+          if ("isExport" in node && node.isExport) {
+            // export import foo = require('./foo');
+            // export import Bar = Foo.Bar;
+            exports.add(node.id.name);
+          }
+          break;
+      }
+    },
+  });
+  return {
+    exports: [...exports],
+    types: [...types],
+  };
 }
 
 /** @type {import("eslint").Rule.RuleModule} */
@@ -399,6 +393,12 @@ module.exports = {
       category: "Possible Errors",
       recommended: true,
       url: require("../../package.json").homepage,
+    },
+    messages: {
+      [E_EXPORT_ALL]:
+        "Prefer explicit exports over `export *` to avoid name clashes, and improve tree-shakeability.",
+      [E_NO_NAMED_EXPORTS]:
+        "`export *` has no effect as there are no named exports to re-export.",
     },
     fixable: "code",
     schema: [
@@ -427,32 +427,35 @@ module.exports = {
         }
 
         const result = extractExports(ruleContext, source, maxDepth);
+        const hasNamedExports = !isEmpty(result);
+
         context.report({
           node,
-          message:
-            "Prefer explicit exports over `export *` to avoid name clashes, and improve tree-shakeability.",
-          fix: isEmpty(result)
-            ? null
-            : (fixer) => {
-                /** @type {string[]} */
-                const lines = [];
-                if (result.types.length > 0) {
-                  const uniqueTypes = result.types.filter(
-                    (type) => !result.exports.includes(type)
-                  );
-                  if (uniqueTypes.length > 0) {
-                    const types = uniqueTypes.sort().join(", ");
-                    lines.push(
-                      `export type { ${types} } from ${node.source.raw};`
-                    );
-                  }
-                }
-                if (result.exports.length > 0) {
-                  const names = result.exports.sort().join(", ");
-                  lines.push(`export { ${names} } from ${node.source.raw};`);
-                }
-                return fixer.replaceText(node, lines.join("\n"));
-              },
+          messageId: hasNamedExports ? E_EXPORT_ALL : E_NO_NAMED_EXPORTS,
+          fix: (fixer) => {
+            if (!hasNamedExports) {
+              // We currently cannot fix this automatically as there are side
+              // effects and other cases to consider. For now, just report it.
+              return null;
+            }
+
+            /** @type {string[]} */
+            const lines = [];
+            if (result.types.length > 0) {
+              const uniqueTypes = result.types.filter(
+                (type) => !result.exports.includes(type)
+              );
+              if (uniqueTypes.length > 0) {
+                const types = uniqueTypes.sort().join(", ");
+                lines.push(`export type { ${types} } from ${node.source.raw};`);
+              }
+            }
+            if (result.exports.length > 0) {
+              const names = result.exports.sort().join(", ");
+              lines.push(`export { ${names} } from ${node.source.raw};`);
+            }
+            return fixer.replaceText(node, lines.join("\n"));
+          },
         });
       },
     };
