67fe0d66ec7a673936291e2d8a5c9c8b5bf5c314
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-01-13 18:13:35 +0100

    fix(metro-resolver-symlinks): fix types not being exported (#3921)

---
+++ .changeset/floppy-moons-roll.md
@@ -0,0 +1,5 @@
+---
+"@rnx-kit/metro-resolver-symlinks": patch
+---
+
+Fixed no types being exported
--- packages/metro-resolver-symlinks/src/index.ts
+++ packages/metro-resolver-symlinks/src/index.ts
@@ -1,3 +1,6 @@
 import { makeResolver } from "./symlinkResolver.ts";
 
 module.exports = makeResolver;
+
+// eslint-disable-next-line no-restricted-exports
+export default makeResolver;
