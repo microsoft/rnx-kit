1f34fd154a2461af3e8401a8a5a03604a954d4fc
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-01-21 13:01:03 +0100

    fix(react-native-host): `useShadowNodeStateOnClone` is removed in 0.85 (#3930)

--- packages/react-native-host/cocoa/RNXFeatureMacros.h
+++ packages/react-native-host/cocoa/RNXFeatureMacros.h
@@ -16,6 +16,15 @@
 
 #if USE_BRIDGELESS
 
+#if __has_include(<cxxreact/ReactNativeVersion.h>)
+#include <cxxreact/ReactNativeVersion.h>
+#define REACT_NATIVE_VERSION                                                                       \
+    REACT_NATIVE_VERSION_MAJOR * 1000000 + REACT_NATIVE_VERSION_MINOR * 1000 +                     \
+        REACT_NATIVE_VERSION_PATCH
+#else
+#define REACT_NATIVE_VERSION 0
+#endif  // __has_include(<cxxreact/ReactNativeVersion.h>)
+
 #if __has_include(<react/config/ReactNativeConfig.h>)
 #define USE_REACT_NATIVE_CONFIG 1
 #endif  // __has_include(<react/config/ReactNativeConfig.h>)
@@ -38,7 +47,8 @@
 #define USE_VIEW_COMMAND_RACE_FIX 1
 #endif  // !__has_include(<React-RCTAppDelegate/RCTReactNativeFactory.h>)
 
-#if __has_include(<React-RCTAppDelegate/RCTJSRuntimeConfiguratorProtocol.h>) || __has_include(<React_RCTAppDelegate/RCTJSRuntimeConfiguratorProtocol.h>)
+// `useShadowNodeStateOnClone` should be enabled from 0.79 and is on by default in 0.85
+#if REACT_NATIVE_VERSION < 85000 && (__has_include(<React-RCTAppDelegate/RCTJSRuntimeConfiguratorProtocol.h>) || __has_include(<React_RCTAppDelegate/RCTJSRuntimeConfiguratorProtocol.h>))
 #define USE_UPDATE_RUNTIME_SHADOW_NODE_REFS_ON_COMMIT 1
 #endif  // __has_include(<React-RCTAppDelegate/RCTJSRuntimeConfiguratorProtocol.h>)
 
