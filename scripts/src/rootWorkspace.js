bef6b593c6a212f8fc33e0d9e065deb826b30143
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-02-06 12:52:44 +0100

    chore: opt-in to TypeScript native preview

--- scripts/src/rootWorkspace.js
+++ scripts/src/rootWorkspace.js
@@ -1,13 +1,10 @@
 // @ts-check
-import * as fs from "node:fs";
-import { URL } from "node:url";
+import manifest from "../../package.json" with { type: "json" };
 
 /**
  * @returns {Record<string, string>}
  */
 export function getRootEnginesField() {
-  const root = fs.readFileSync(new URL("../../package.json", import.meta.url));
-  const manifest = JSON.parse(root.toString());
   if (typeof manifest.engines !== "object") {
     throw new Error("'engines' field is incorrectly configured");
   }
