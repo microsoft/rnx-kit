552f4810959c645874701e47ee2c6c410e610623
Jason Morse <jasonmo@microsoft.com>
2026-02-20 11:04:51 -0800

    fix esbuild types for the plugin types package

--- packages/types-metro-serializer-esbuild/src/index.ts
+++ packages/types-metro-serializer-esbuild/src/index.ts
@@ -1,19 +1,34 @@
-import type { BuildOptions } from "esbuild";
+/**
+ * Plugin options for @rnx-kit/metro-serializer-esbuild.
+ *
+ * These options are a combinations of a subset of esbuild's BuildOptions and additional options specific to the plugin.
+ */
+
+/**
+ * Some esbuild types used in the options
+ */
+type Drop = "console" | "debugger";
+type LogLevel = "verbose" | "debug" | "info" | "warning" | "error" | "silent";
+
+/**
+ * Options redefined from esbuild's BuildOptions, separated so type level compatibility
+ * can be checked at build time
+ */
+export type BaseBuildOptions = {
+  drop?: Drop[];
+  logLevel?: LogLevel;
+  minify?: boolean;
+  minifyWhitespace?: boolean;
+  minifyIdentifiers?: boolean;
+  minifySyntax?: boolean;
+  pure?: string[];
+  target?: string | string[];
+};
 
 /**
  * Options for @rnx-kit/metro-serializer-esbuild plugin.
  */
-export type SerializerEsbuildOptions = Pick<
-  BuildOptions,
-  | "drop"
-  | "logLevel"
-  | "minify"
-  | "minifyWhitespace"
-  | "minifyIdentifiers"
-  | "minifySyntax"
-  | "pure"
-  | "target"
-> & {
+export type SerializerEsbuildOptions = BaseBuildOptions & {
   analyze?: boolean | "verbose";
   fabric?: boolean;
   metafile?: string;
