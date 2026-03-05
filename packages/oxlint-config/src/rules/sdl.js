5ce018da126ee81df86c2e68002d446781cc76f9
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-03-05 12:47:42 +0100

    @microsoft/eslint-plugin-sdl does not have types

--- packages/oxlint-config/src/rules/sdl.js
+++ packages/oxlint-config/src/rules/sdl.js
@@ -1,12 +1,22 @@
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noCookies from "@microsoft/eslint-plugin-sdl/lib/rules/no-cookies.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noDocumentDomain from "@microsoft/eslint-plugin-sdl/lib/rules/no-document-domain.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noDocumentWrite from "@microsoft/eslint-plugin-sdl/lib/rules/no-document-write.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noHtmlMethod from "@microsoft/eslint-plugin-sdl/lib/rules/no-html-method.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noInnerHtml from "@microsoft/eslint-plugin-sdl/lib/rules/no-inner-html.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noInsecureUrl from "@microsoft/eslint-plugin-sdl/lib/rules/no-insecure-url.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noMsappExecUnsafe from "@microsoft/eslint-plugin-sdl/lib/rules/no-msapp-exec-unsafe.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noPostmessageStarOrigin from "@microsoft/eslint-plugin-sdl/lib/rules/no-postmessage-star-origin.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noUnsafeAlloc from "@microsoft/eslint-plugin-sdl/lib/rules/no-unsafe-alloc.js";
+// @ts-expect-error @microsoft/eslint-plugin-sdl does not have types
 import noWinjsHtmlUnsafe from "@microsoft/eslint-plugin-sdl/lib/rules/no-winjs-html-unsafe.js";
 
 /**
