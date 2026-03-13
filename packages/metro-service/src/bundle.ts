cf231fe6bbd623f8ec1fcc181b0e2c6f29f04a12
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2026-03-13 13:57:24 +0100

    fixup! fix(metro-service): fix `metro` not being imported correctly

--- packages/metro-service/src/bundle.ts
+++ packages/metro-service/src/bundle.ts
@@ -61,7 +61,8 @@ export async function bundle(
   // `runMetro` was introduced in 0.71:
   // https://github.com/facebook/metro/commit/a0f99e136fbd2e02ab070437cee9f6e9baa36d16
   const options = { paths: [config.projectRoot] };
-  const { runMetro } = await import(require.resolve(metroPath, options));
+  const metroUrl = `file://${require.resolve(metroPath, options).replaceAll("\\", "/")}`;
+  const { runMetro } = await import(metroUrl);
   if (!runMetro) {
     const { buildBundle } = await import("./bundle/bundle-0.66.js");
     return await buildBundle(args, config, output, {
